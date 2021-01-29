fe2da9e45382ae00d7fae95b3d5d68731ac8769b
Lennard Behrens <behrenle@posteo.de>
2021-01-29 16:11:15 +0100

    types and some bundler testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+import {Number} from "types";
+import { createNumber } from "./create";
+
+const foo: Number = {
+    type: "Number",
+    value: 3.14
+}
+
+console.log(foo);
+console.log(createNumber(4));
