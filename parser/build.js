1a7f87b951cdf606f754e858c3e9497fc33b12d4
Lennard Behrens <behrenle@posteo.de>
2021-01-31 22:48:41 +0100

    build parser from grammar with pegjs

---
+++ parser/build.js
@@ -0,0 +1,12 @@
+import pegjs from "pegjs";
+import fs from "fs";
+
+const pathToGrammar = "parser/grammar.peg";
+const outputPath = "src/parser.js";
+const grammarSource = fs.readFileSync(pathToGrammar, 'utf8');
+const parserSource = pegjs.generate(grammarSource, {
+    output: "source",
+    format: "commonjs",
+});
+
+fs.writeFileSync(outputPath, parserSource);
