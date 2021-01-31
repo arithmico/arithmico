7ada23d414e2bba696b76e7f124802f72f2882c0
Lennard Behrens <behrenle@posteo.de>
2021-01-31 14:00:31 +0100

    re-export create functions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import {Number} from "./types.js";
-import {createNumber} from "./create/create.js";
-
-const foo: Number = {
-    type: "Number",
-    value: 3.14
-}
-
-console.log(foo);
-console.log(createNumber(4));
+export * as create from "./create/create.js";
