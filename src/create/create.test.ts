8b319ff9c51438c41bdd009c8761c87d09bec72c
Lennard Behrens <behrenle@posteo.de>
2021-02-02 18:03:45 +0100

    create primitive tests

--- src/create/create.test.ts
+++ src/create/create.test.ts
@@ -1,8 +1,26 @@
-import { createNumber } from "./create.js"
+import { createBoolean, createNumber, createSymbol } from "./create.js"
 
-test("create-number", () => {
-    expect(createNumber(1.2)).toStrictEqual({
-        type: "Number",
-        value: 1.2
+const compareNodes = (node1: any, node2: any) => expect(node1).toStrictEqual(node2);
+
+describe("create-primitive", () => {
+    test("create-number", () => {
+        compareNodes(createNumber(1.2), {
+            type: "Number",
+            value: 1.2
+        });
+    });
+    
+    test("create-boolean", () => {
+        compareNodes(createSymbol("x"), {
+            type: "Symbol",
+            name: "x"
+        });
+    });
+
+    test("create-boolean", () => {
+        compareNodes(createBoolean(true), {
+            type: "Boolean",
+            value: true
+        });
     });
 });
