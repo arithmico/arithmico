7911296d826152edebc3c29c42c5abce676300c2
Lennard Behrens <lb@thepeaklab.com>
2021-09-05 17:06:38 +0200

    fixed file names

--- src/serialize/index.ts
+++ src/serialize/index.ts
@@ -1,7 +1,15 @@
+import {Serializer, SerializerSet} from "./types.js";
 import {Context, SyntaxTreeNode} from "../types.js";
 
-export type SerializerSet = {
-    [key in SyntaxTreeNode["type"]]?: Serializer;
+let serializerSet: SerializerSet = {};
+
+export const registerSerializer = (target: SyntaxTreeNode["type"], serializer: Serializer) => {
+    serializerSet[target] = serializer;
 };
 
-export type Serializer = (node: SyntaxTreeNode, options: Context["options"]) => string;
+export const serialize = (node: SyntaxTreeNode, options: Context["options"]): string => {
+    if (!serializerSet[node.type])
+        throw `no serializer available for type "${node.type}"`;
+
+    return serializerSet[node.type](node, options);
+};
--- src/serialize/types.ts
+++ src/serialize/types.ts
@@ -1,15 +1,7 @@
-import {Serializer, SerializerSet} from "./index.js";
 import {Context, SyntaxTreeNode} from "../types.js";
 
-let serializerSet: SerializerSet = {};
-
-export const registerSerializer = (target: SyntaxTreeNode["type"], serializer: Serializer) => {
-    serializerSet[target] = serializer;
+export type SerializerSet = {
+    [key in SyntaxTreeNode["type"]]?: Serializer;
 };
 
-export const serialize = (node: SyntaxTreeNode, options: Context["options"]): string => {
-    if (!serializerSet[node.type])
-        throw `no serializer available for type "${node.type}"`;
-
-    return serializerSet[node.type](node, options);
-};
+export type Serializer = (node: SyntaxTreeNode, options: Context["options"]) => string;
