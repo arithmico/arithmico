f738cbf1196162f59df3e36b4b95dfe581477ce3
Lennard Behrens <lb@thepeaklab.com>
2022-02-11 22:21:07 +0100

    refactored: move calculator-textfields, calculator-toolbar to dedicated component directories

--- src/pages/calculator/calculator.tsx
+++ src/pages/calculator/calculator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import CalculatorTextfields from './calculator-textfields';
-import CalculatorToolbar from './calculator-toolbar';
+import CalculatorTextfields from '../../calculator-textfields/calculator-textfields';
+import CalculatorToolbar from '../../calculator-toolbar/calculator-toolbar';
 
 const Container = styled.div`
   display: flex;
