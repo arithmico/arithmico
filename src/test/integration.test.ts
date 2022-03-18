b38818874d87d76d0b09cc8aab623392d860b62b
Lennard Behrens <behrenle@posteo.de>
2022-03-18 18:27:58 +0100

    test: add integration tests

--- src/test/integration.test.ts
+++ src/test/integration.test.ts
@@ -1,10 +1,25 @@
 import evaluate from '..';
+import createNumberNode from '../create/NumberNode';
+import { Context, Options } from '../types';
+
+const testOptions: Options = {
+    decimalPlaces: 6,
+    decimalSeparator: '.',
+    magnitudeThresholdForScientificNotation: 6,
+};
+
+function createTestContext(stack: Context['stack'], options: Options = testOptions): Context {
+    return {
+        stack,
+        options,
+    };
+}
 
 let lastId = 0;
 
-function integrationTest(input: string, expectedOutput: string) {
+function integrationTest(input: string, expectedOutput: string, context?: Context) {
     test(`integration test #${++lastId}`, () => {
-        expect(evaluate(input)).toBe(expectedOutput);
+        expect(evaluate(input, context)).toBe(expectedOutput);
     });
 }
 
@@ -14,6 +29,8 @@ function integrationTestThrow(input: string) {
     });
 }
 
+integrationTest('1', '1');
+integrationTest('[1,2,3]', '[1, 2, 3]');
 integrationTest('1 + 2', '3');
 integrationTest('1 - 2', '-1');
 integrationTest('-2', '-2');
@@ -32,15 +49,24 @@ integrationTest('1 < 2 <= 2 = 2 >= 2 > 1', 'true');
 integrationTest('2 + 3 * 4 ^ 5 / 32 = 98', 'true');
 integrationTest('10 / 2 / 5', '1');
 integrationTest('10 - 5 - 3 - 2', '0');
-integrationTest('[[1, 2], 3] + [[3, 2], 1]', '[4, 4, 4]');
-
-test('foo', () => {
-    console.log('foo');
-    expect(evaluate('[[1, 2], 3] + [[3, 2], 1]')).toBe('[4, 4, 4]');
-}, 500);
+integrationTest('[[1, 2], 3] + [[3, 2], 1]', '[[4, 4], 4]');
+integrationTest(
+    'a + 1',
+    '42',
+    createTestContext([
+        {
+            a: {
+                type: 'value',
+                value: createNumberNode(41),
+            },
+        },
+    ]),
+);
 
 integrationTestThrow('1 + true');
 integrationTestThrow('2 + [1,2,3]');
 integrationTestThrow('2 ^ 3 ^ 4');
 integrationTestThrow('4 * true');
 integrationTestThrow('2 * -2');
+integrationTestThrow('[[1,2],3]+[1,2,3]');
+integrationTestThrow('[1,2]+[1,2,3]');
