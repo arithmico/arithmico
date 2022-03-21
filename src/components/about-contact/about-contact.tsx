35bcedd27a609fa9c3a9726b8ad194f7826ea15b
Lennard Behrens <behrenle@posteo.de>
2022-03-21 21:56:41 +0100

    refactor: use external link component

--- src/components/about-contact/about-contact.tsx
+++ src/components/about-contact/about-contact.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import ExternalLink from '../external-link/external-link';
 
 const StyledAddress = styled.address`
   display: flex;
   flex-direction: column;
   font-style: normal;
-
-  & > a {
-    text-decoration: none;
-    color: white;
-    opacity: 0.6;
-  }
 `;
 
 interface AboutContactProps {
@@ -23,18 +18,8 @@ export default function AboutContact({ name, email, url }: AboutContactProps) {
   return (
     <StyledAddress>
       <span>{name}</span>
-      {email ? (
-        <>
-          <a href={`mailto:${email}`}>{email}</a>
-        </>
-      ) : null}
-      {url ? (
-        <>
-          <a href={url} target="_blank" rel="noreferrer">
-            {url}
-          </a>
-        </>
-      ) : null}
+      {email ? <ExternalLink href={`mailto:${email}`}>{email}</ExternalLink> : null}
+      {url ? <ExternalLink href={url}>{url}</ExternalLink> : null}
     </StyledAddress>
   );
 }
