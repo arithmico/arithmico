aa35bb82845a52fb42f3d491ecbff8ab595a7fbe
Lennard Behrens <behrenle@posteo.de>
2022-03-21 21:32:57 +0100

    feat: show number-cruncher function and constant documentation on manual page

--- src/pages/manual/manual.tsx
+++ src/pages/manual/manual.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { getDocumentation } from '@behrenle/number-cruncher';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ManualSectionItem from '../../components/manual-section-item/manual-section-item';
 import ManualSection from '../../components/manual-section/manual-section';
@@ -8,18 +9,21 @@ const Container = styled.div`
 `;
 
 const SearchField = styled.input.attrs({ type: 'text' })`
+  background-color: rgba(255, 255, 255, 0.05);
   height: 100px;
   border: 2px solid white;
   border-radius: 10px;
   width: 100%;
   outline: none;
-  background-color: transparent;
+  //background-color: transparent;
   color: white;
   font-size: 24px;
   padding: 20px;
 `;
 
 export default function Manual() {
+  const [documentation] = useState(() => getDocumentation());
+
   return (
     <Container>
       <SearchField placeholder="Search" />
@@ -27,13 +31,26 @@ export default function Manual() {
         <ManualSectionItem synopsis="ALT + I" description="Focus input field" />
       </ManualSection>
       <ManualSection heading="Constants">
-        <ManualSectionItem synopsis="e" description="Euler's Number" />
+        {documentation
+          .filter((item) => item.type === 'constant')
+          .map((item) => (
+            <ManualSectionItem
+              key={item.documentation.en?.synopsis}
+              synopsis={item.documentation.en?.synopsis || ''}
+              description={item.documentation.en?.description || ''}
+            />
+          ))}
       </ManualSection>
       <ManualSection heading="Functions">
-        <ManualSectionItem
-          synopsis="sin(x)"
-          description="Calculates the sine of a given value x."
-        />
+        {documentation
+          .filter((item) => item.type === 'function')
+          .map((item) => (
+            <ManualSectionItem
+              key={item.documentation.en?.synopsis}
+              synopsis={item.documentation.en?.synopsis || ''}
+              description={item.documentation.en?.description || ''}
+            />
+          ))}
       </ManualSection>
     </Container>
   );
