449837186bcb330f857ada2877934c8190c31046
Lennard Behrens <behrenle@posteo.de>
2022-03-21 23:48:44 +0100

    feat: hide those ugly scrollbars

--- src/components/page-container/page-container.tsx
+++ src/components/page-container/page-container.tsx
@@ -2,8 +2,14 @@ import styled from 'styled-components';
 
 const PageContainer = styled.div`
   padding: 50px 20%;
-  overflow-y: auto;
+  overflow-y: scroll;
   overflow-x: hidden;
+  -ms-overflow-style: none; /* for Internet Explorer, Edge */
+  scrollbar-width: none; /* for Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* for Chrome, Safari, and Opera */
+  }
 `;
 
 export default PageContainer;
