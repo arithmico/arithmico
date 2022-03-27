0c1aab9310bfdd32580cd658162772c087ab2536
Lennard Behrens <behrenle@posteo.de>
2022-03-27 17:10:07 +0200

    feat: persist partial session store

--- src/stores/session-store/use-session-store.ts
+++ src/stores/session-store/use-session-store.ts
@@ -1,24 +1,35 @@
 import { init, getDefaultContext } from '@behrenle/number-cruncher';
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 import { evaluateInput, resetDefinitions } from './actions';
 import { SessionState } from './types';
 
 init();
 
-const useSessionStore = create<SessionState>((set) => ({
-  input: '',
-  outputResetted: false,
-  protocol: [],
-  stack: getDefaultContext().stack,
-  decimalPlaces: getDefaultContext().options.decimalPlaces,
+const useSessionStore = create<SessionState>(
+  persist(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (set, _get) => ({
+      input: '',
+      outputResetted: false,
+      protocol: [],
+      stack: getDefaultContext().stack,
+      decimalPlaces: getDefaultContext().options.decimalPlaces,
 
-  evaluate: () => set(evaluateInput),
-  resetDefinitions: () => set(resetDefinitions),
-  setInput: (input: string) => set(() => ({ input })),
-  resetInput: () => set(() => ({ input: '' })),
-  resetOutput: () => set(() => ({ outputResetted: true })),
-  resetProtocol: () => set(() => ({ protocol: [] })),
-  setDecimalPlaces: (n: number) => set(() => ({ decimalPlaces: n }))
-}));
+      evaluate: () => set(evaluateInput),
+      resetDefinitions: () => set(resetDefinitions),
+      setInput: (input: string) => set(() => ({ input })),
+      resetInput: () => set(() => ({ input: '' })),
+      resetOutput: () => set(() => ({ outputResetted: true })),
+      resetProtocol: () => set(() => ({ protocol: [] })),
+      setDecimalPlaces: (n: number) => set(() => ({ decimalPlaces: n }))
+    }),
+    {
+      name: 'settings',
+      version: 1,
+      partialize: (state) => ({ decimalPlaces: state.decimalPlaces })
+    }
+  )
+);
 
 export default useSessionStore;
