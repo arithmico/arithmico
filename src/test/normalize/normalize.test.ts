bd60f9d86686f54a5eb071f011c928e6cf39da17
Lennard Behrens <behrenle@posteo.de>
2022-04-28 19:40:01 +0200

    test: add normalize tests

---
+++ src/test/normalize/normalize.test.ts
@@ -0,0 +1,31 @@
+import { Context, Options } from './../../types';
+import { parse } from '../../parse/parser';
+import normalize from '../../normalize';
+import serialize from '../../serialize';
+
+const testOptions: Options = {
+    decimalPlaces: 6,
+    decimalSeparator: '.',
+    magnitudeThresholdForScientificNotation: 6,
+};
+
+const testContext: Context = {
+    options: testOptions,
+    stack: [{}],
+};
+
+let lastId = 0;
+
+function normalizeTest(input: string, expectedOutput: string, context: Context = testContext) {
+    test(`normalize test #${++lastId}`, () => {
+        const node = parse(input, testOptions);
+        const normalizedNode = normalize(node, context);
+        expect(serialize(normalizedNode, context.options)).toBe(expectedOutput);
+    });
+}
+
+normalizeTest('42', '42');
+normalizeTest('true', 'true');
+normalizeTest('x', 'x');
+normalizeTest('x + 2', '2 + x');
+normalizeTest('2 + 2', '4');
