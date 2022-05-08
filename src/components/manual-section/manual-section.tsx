2701de41cb1b1a659bf38a06f3b61288170474a1
Lennard Behrens <lb@thepeaklab.com>
2022-05-08 20:46:24 +0200

    feat: change manual section grid layout

--- src/components/manual-section/manual-section.tsx
+++ src/components/manual-section/manual-section.tsx
@@ -10,7 +10,7 @@ const ManaulSectionHeading = styled.h1`
 
 const StyledDl = styled.dl`
   display: grid;
-  grid-template-columns: 1fr 3fr;
+  grid-template-columns: 1fr 1fr;
   grid-row-gap: 2em;
   grid-column-gap: 1em;
   padding: 10px 80px;
