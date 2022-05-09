52ed8197d941606761ea57d675a274c8f3725543
Lennard Behrens <lb@thepeaklab.com>
2022-05-09 18:03:24 +0200

    fix: fix build process

--- parser/build.js
+++ parser/build.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const peggy = require('peggy');
 const fs = require('fs');
 
 const pathToGrammar = 'parser/grammar.peggy';
+const outputDir = 'src/parse';
 const outputPath = 'src/parse/parser.js';
 const grammarSource = fs.readFileSync(pathToGrammar, 'utf8');
 const parserSource = peggy.generate(grammarSource, {
@@ -10,4 +13,5 @@ const parserSource = peggy.generate(grammarSource, {
     cache: true,
 });
 
+fs.mkdirSync(outputDir);
 fs.writeFileSync(outputPath, parserSource);
