662b401b921bb2d4970579c4e3dfe5daa41d3fa7
Lennard Behrens <behrenle@posteo.de>
2022-05-24 22:25:48 +0200

    first prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Chapter from "./pages/chapter/chapter";
+import Page from "./components/page/page";
+import { Route, Routes } from "react-router-dom";
+import chaptersDe from "./chapters";
+
+const Container = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Page>Wilkommen</Page>} />
+        {chaptersDe.map((chapter, index) => (
+          <Route
+            path={`/chapters/${index}`}
+            element={
+              <Chapter
+                key={index}
+                contentUrl={chapter.url}
+                title={chapter.title}
+                nextChapter={
+                  index < chaptersDe.length - 1 ? index + 1 : undefined
+                }
+                previousChapter={index > 0 ? index - 1 : undefined}
+              />
+            }
+          />
+        ))}
+      </Routes>
+    </Container>
   );
 }
 
