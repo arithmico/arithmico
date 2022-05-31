3e30c874ae1f23cc2525b4b2bcef4bdd9faff7e2
Lennard Behrens <behrenle@posteo.de>
2022-05-31 17:18:59 +0200

    fix: fix scrolling issues

--- src/components/page-container/page-container.tsx
+++ src/components/page-container/page-container.tsx
@@ -2,8 +2,6 @@ import styled from 'styled-components';
 
 const PageContainer = styled.div`
   padding: 50px 20%;
-  overflow-y: scroll;
-  overflow-x: hidden;
   -ms-overflow-style: none; /* for Internet Explorer, Edge */
   scrollbar-width: none; /* for Firefox */
 
