06b4fdca26537550abbd960136948adf39fc3c0a
Lennard Behrens <behrenle@posteo.de>
2022-06-08 15:19:25 +0200

    fix: fix exists condition

--- parser/build.js
+++ parser/build.js
@@ -13,7 +13,7 @@ const parserSource = peggy.generate(grammarSource, {
     cache: true,
 });
 
-if (fs.existsSync(!outputDir)) {
+if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir);
 }
 fs.writeFileSync(outputPath, parserSource);
