6b12a358fa031f9dbe92f3ebbff403171448b693
Lennard Behrens <behrenle@posteo.de>
2022-06-26 16:45:39 +0200

    fix: fix error

--- tools/auto-version-dev.sh
+++ tools/auto-version-dev.sh
@@ -1,4 +1,4 @@
-mkdir tmp
+mkdir -p tmp
 last_version_commit_hash=$(git log --pretty=format:"%H %s" | grep -m 1 -E "\b[0-9]+\.[0-9]+.[0-9]+\b" | cut -d " " -f1)
 git log --pretty=format:"%s" $last_version_commit_hash..HEAD > tmp/commits.txt
 cat tmp/commits.txt
