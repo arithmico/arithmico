103b0b06ddaefc98e482ee27b8130f3fd70f02e9
Lennard Behrens <behrenle@posteo.de>
2022-06-26 16:44:36 +0200

    feat: save commits messages in tmp file

--- tools/auto-version-dev.sh
+++ tools/auto-version-dev.sh
@@ -1,3 +1,4 @@
-last_version_commit_hash=git log --pretty=format:"%H %s" | grep -m 1 -E "\b[0-9]+\.[0-9]+.[0-9]+\b" | cut -d " " -f1
-commits_after_last_version=git log --pretty=format:"%s" $last_version_commit_hash..HEAD
-echo $commits_after_last_version
+mkdir tmp
+last_version_commit_hash=$(git log --pretty=format:"%H %s" | grep -m 1 -E "\b[0-9]+\.[0-9]+.[0-9]+\b" | cut -d " " -f1)
+git log --pretty=format:"%s" $last_version_commit_hash..HEAD > tmp/commits.txt
+cat tmp/commits.txt
