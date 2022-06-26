fe48a41b25da642742cf7b82eee5c5a7156c229a
Lennard Behrens <behrenle@posteo.de>
2022-06-26 16:55:25 +0200

    feat: integrate auto version

--- tools/auto-version-dev.sh
+++ tools/auto-version-dev.sh
@@ -1,4 +1,11 @@
 mkdir -p tmp
 last_version_commit_hash=$(git log --pretty=format:"%H %s" | grep -m 1 -E "\b[0-9]+\.[0-9]+.[0-9]+\b" | cut -d " " -f1)
 git log --pretty=format:"%s" $last_version_commit_hash..HEAD > tmp/commits.txt
-cat tmp/commits.txt
+
+if cat tmp/commits.txt | grep "breaking:"; then
+    npm version premajor
+elif ! cat tmp/commits.txt | grep "breaking:" && cat tmp/commits.txt | grep "feat:"; then
+    npm version preminor
+elif ! cat tmp/commits.txt | grep "breaking:" && ! cat tmp/commits.txt | grep "feat:"; then
+    npm version prepatch
+fi
