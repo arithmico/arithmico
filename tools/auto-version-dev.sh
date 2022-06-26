26901bd343a81f9ff5b789fcae2a1b8bc0ae255e
Lennard Behrens <behrenle@posteo.de>
2022-06-26 22:04:56 +0200

    feat: add auto version logs

--- tools/auto-version-dev.sh
+++ tools/auto-version-dev.sh
@@ -3,9 +3,12 @@ last_version_commit_hash=$(git log --pretty=format:"%H %s" | grep -m 1 -E "\b[0-
 git log --pretty=format:"%s" $last_version_commit_hash..HEAD > tmp/commits.txt
 
 if cat tmp/commits.txt | grep "breaking:"; then
+    echo "major version bump"
     npm version premajor
 elif ! cat tmp/commits.txt | grep "breaking:" && cat tmp/commits.txt | grep "feat:"; then
+    echo "minor version bump"
     npm version preminor
 elif ! cat tmp/commits.txt | grep "breaking:" && ! cat tmp/commits.txt | grep "feat:"; then
+    echo "patch version bump"
     npm version prepatch
 fi
