3a06d9a1ee848c229b393acd64a2796a80355e18
Lennard Behrens <behrenle@posteo.de>
2022-06-27 11:42:04 +0200

    feat: move autoversion to main branch workflow

--- tools/auto-version.sh
+++ tools/auto-version.sh
@@ -4,11 +4,11 @@ git log --pretty=format:"%s" $last_version_commit_hash..HEAD > tmp/commits.txt
 
 if cat tmp/commits.txt | grep "breaking:"; then
     echo "major version bump"
-    npm version premajor
+    npm version major
 elif ! cat tmp/commits.txt | grep "breaking:" && cat tmp/commits.txt | grep "feat:"; then
     echo "minor version bump"
-    npm version preminor
+    npm version minor
 elif ! cat tmp/commits.txt | grep "breaking:" && ! cat tmp/commits.txt | grep "feat:"; then
     echo "patch version bump"
-    npm version prepatch
+    npm version patch
 fi
