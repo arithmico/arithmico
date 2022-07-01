d68e8c2d48f18656780d38c6a147f85ed90e60c6
Lennard Behrens <behrenle@posteo.de>
2022-07-01 17:13:53 +0200

    feat: inital plugin config component prototype

--- apps/config/src/pages/home/home.tsx
+++ apps/config/src/pages/home/home.tsx
@@ -1,6 +1,20 @@
 import React from "react";
+import PluginConfig from "../../components/plugin-config/plugin-config";
 import Page from "../../components/page/page";
 
 export default function Home() {
-  return <Page>Welcome to Arithmico Config</Page>;
+  return (
+    <Page>
+      <PluginConfig
+        name="foo"
+        items={[
+          {
+            name: "foo_bar",
+            description: "example description",
+            synopsis: "FOO_BAR",
+          },
+        ]}
+      />
+    </Page>
+  );
 }
