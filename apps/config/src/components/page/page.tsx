c6a91a1582a81034875f10ac1cef7c048e832151
Lennard Behrens <behrenle@posteo.de>
2022-07-01 16:37:56 +0200

    feat: add arithmico config app

---
+++ apps/config/src/components/page/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import HeaderNavBar from "@components/header-nav-bar/header-nav-bar";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-rows: auto 1fr;
+`;
+
+const Content = styled.main`
+  margin-top: 1.5rem;
+`;
+
+const ScrollWrapper = styled.div`
+  overflow-y: auto;
+  display: grid;
+  padding: 0 15%;
+`;
+
+interface PageProps {
+  children: React.ReactNode;
+}
+
+export default function Page({ children }: PageProps) {
+  return (
+    <Container>
+      <HeaderNavBar title="Arithmico" subTitle="Config" />
+      <ScrollWrapper>
+        <Content>{children}</Content>
+      </ScrollWrapper>
+    </Container>
+  );
+}
---
+++ apps/config/src/pages/home/home.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Page from "../../components/page/page";
+
+export default function Home() {
+  return <Page>Welcome to Arithmico Config</Page>;
+}
