d006b33a279ec7b67edce40e6dce00b499542706
Lennard Behrens <behrenle@posteo.de>
2022-07-22 00:44:54 +0200

    feat: open/close plugin config disclosure

--- apps/config/src/components/plugin-config/plugin-config.tsx
+++ apps/config/src/components/plugin-config/plugin-config.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PluginObjectToggle from "../plugin-object-toggle/plugin-object-toggle";
 import styled from "styled-components";
+import { Disclosure } from "@headlessui/react";
+import ExpandMore from "@components/icons/expand-more";
 
 interface PluginItemProps {
   name: string;
@@ -8,17 +10,18 @@ interface PluginItemProps {
   description: string;
 }
 
-interface PluginConfigProps {
-  name: string;
-  items: PluginItemProps[];
-}
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2em;
+  margin-bottom: 0.5em;
+`;
 
 const PluginTitle = styled.h1`
   font-size: 2.5em;
   font-weight: var(--me-font-weight-normal);
-  margin-bottom: 0.5em;
-  margin-top: 2em;
   color: var(--me-text-400);
+  margin: 0;
 `;
 
 const PluginObjectsList = styled.ul`
@@ -27,20 +30,59 @@ const PluginObjectsList = styled.ul`
   margin: 0;
 `;
 
+const DisclosureButton = styled(Disclosure.Button)<{
+  open: boolean;
+  children: React.ReactNode; // idk why styled components does not get the children property
+}>`
+  display: flex;
+  align-items: center;
+  padding: 0;
+  margin: 0;
+  background-color: transparent;
+  border: none;
+  border-bottom: ${({ open }) =>
+    open ? "none" : "1px solid var(--me-text-100)"};
+  outline: none;
+`;
+
+const ExpandMoreIcon = styled(ExpandMore)<{
+  open: boolean;
+}>`
+  transform: rotate(${({ open }) => (open ? "180" : "0")}deg);
+  margin-left: auto;
+  transition: transform 0.25s;
+`;
+
+interface PluginConfigProps {
+  name: string;
+  items: PluginItemProps[];
+}
+
 export default function PluginConfig({ name, items }: PluginConfigProps) {
   return (
-    <section>
-      <PluginTitle>{name}</PluginTitle>
-      <PluginObjectsList>
-        {items.map((item, index) => (
-          <PluginObjectToggle
-            key={index}
-            enabled
-            label={item.synopsis}
-            onChange={() => null}
-          ></PluginObjectToggle>
-        ))}
-      </PluginObjectsList>
-    </section>
+    <Section>
+      <Disclosure>
+        {({ open }) => (
+          <>
+            <DisclosureButton open={open}>
+              <PluginTitle>{name}</PluginTitle>
+              <ExpandMoreIcon open={open} />
+            </DisclosureButton>
+            <Disclosure.Panel>
+              <PluginObjectsList>
+                {items.map((item, index) => (
+                  <PluginObjectToggle
+                    key={index}
+                    enabled
+                    label={item.synopsis}
+                    onChange={() => null}
+                  ></PluginObjectToggle>
+                ))}
+              </PluginObjectsList>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </Section>
   );
 }
