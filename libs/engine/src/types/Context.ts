7d9f9b1811b1cd0febee2596fb9846f858f78baf
Lennard Behrens <behrenle@posteo.de>
2022-07-21 21:20:06 +0200

    chore: simplify types

--- libs/engine/src/types/Context.ts
+++ libs/engine/src/types/Context.ts
@@ -6,56 +6,7 @@ export interface Options {
     magnitudeThresholdForScientificNotation: number;
     angleUnit: 'degrees' | 'radians';
     operators: {
-        define: boolean;
-        lambda: boolean;
-
-        orBooleanBoolean: boolean;
-        orFunctionFunction: boolean;
-
-        andBooleanBoolean: boolean;
-        andFunctionFunction: boolean;
-
-        negateNumber: boolean;
-        negateBoolean: boolean;
-        negateFunction: boolean;
-
-        equalsNumberNumber: boolean;
-        equalsBooleanBoolean: boolean;
-        equalsFunctionFunction: boolean;
-
-        lessNumberNumber: boolean;
-        lessFunctionFunction: boolean;
-
-        lessOrEqualsNumberNumber: boolean;
-        lessOrEqualsFunctionFunction: boolean;
-
-        greaterNumberNumber: boolean;
-        greaterFunctionFunction: boolean;
-
-        greaterOrEqualsNumberNumber: boolean;
-        greaterOrEqualsFunctionFunction: boolean;
-
-        plusNumberNumber: boolean;
-        plusVectorVector: boolean;
-        plusFunctionFunction: boolean;
-
-        minusNumberNumber: boolean;
-        minusVectorVector: boolean;
-        minusFunctionFunction: boolean;
-
-        timesNumberNumber: boolean;
-        timesNumberVector: boolean;
-        timesVectorVector: boolean;
-        timesVectorMatrix: boolean;
-        timesMatrixMatrix: boolean;
-        timesFunctionFunction: boolean;
-
-        dividedNumberNumber: boolean;
-        dividedVectorNumber: boolean;
-        dividedFunctionFunction: boolean;
-
-        powerNumberNumber: boolean;
-        powerFunctionFunction: boolean;
+        [key: string]: boolean;
     };
 }
 export interface StackFrame {
