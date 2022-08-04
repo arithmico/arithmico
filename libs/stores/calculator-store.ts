815327fd8c968f91c16dc251f4d625ee91ace58c
Lennard Behrens <behrenle@posteo.de>
2022-08-04 14:43:31 +0200

    feat: create calculatorStore

---
+++ libs/stores/calculator-store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import calculatorSessionSlice from "./calc-slices/calculator-session-slice";
+import calculatorSettingsSlice from "./calc-slices/calculator-settings-slice";
+import globalAppearanceSettingsSlice from "./global-slices/appearance-settings-slice";
+import globalEngineSettingsSlice from "./global-slices/engine-settings-slice";
+import globalLanguageSettingsSlice from "./global-slices/langugage-settings-slice";
+
+const calculatorStore = configureStore({
+  reducer: {
+    calculatorSession: calculatorSessionSlice.reducer,
+    calculatorSettings: calculatorSettingsSlice.reducer,
+    globalAppearanceSettings: globalAppearanceSettingsSlice.reducer,
+    globalEngineSettings: globalEngineSettingsSlice.reducer,
+    globalLanguageSettings: globalLanguageSettingsSlice.reducer,
+  },
+});
+
+export default calculatorStore;
