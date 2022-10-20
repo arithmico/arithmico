9b67ec4872484cee8a02cb6b2fbbd68301bd2634
Lennard Behrens <behrenle@posteo.de>
2022-10-20 14:12:00 +0200

    fix: fix calculateVar function

--- libs/engine/src/plugins/core/statistics/utils/var.ts
+++ libs/engine/src/plugins/core/statistics/utils/var.ts
@@ -2,5 +2,5 @@ import { calculateAvg } from './avg';
 
 export function calculateVar(xs: number[]): number {
     const avg = calculateAvg(xs);
-    return xs.map((x) => Math.pow(x - avg, 2)).reduce((a, b) => a + b, 0) / (xs.length - 1);
+    return xs.map((x) => Math.pow(x - avg, 2)).reduce((a, b) => a + b, 0) / xs.length;
 }
