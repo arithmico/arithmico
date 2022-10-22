5daa39f5dea213e49d5df471d68e7cf9acfa231a
Lennard Behrens <behrenle@posteo.de>
2022-10-22 14:39:21 +0200

    fix: fix autoversioning

--- tools/auto-version.sh
+++ tools/auto-version.sh
@@ -2,7 +2,7 @@ mkdir -p tmp
 last_version_commit_hash=$(git log --pretty=format:"%H %s" | grep -m 1 -E "\b[0-9]+\.[0-9]+.[0-9]+\b" | cut -d " " -f1)
 git log --pretty=format:"%s" $last_version_commit_hash..HEAD > tmp/commits.txt
 
-if cat tmp/commits.txt | grep "breaking:"; then
+if cat tmp/commits.txt | grep -i "breaking:"; then
     echo "major version bump"
     npm version major
 elif ! cat tmp/commits.txt | grep "breaking:" && cat tmp/commits.txt | grep "feat:"; then
