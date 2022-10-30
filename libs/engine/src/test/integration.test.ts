036ffac5d4f6d51cfa657b3a42753409c977c81b
Tizian Roth <tizian.roth@freenet.de>
2022-10-30 22:23:30 +0100

    feat: prime:is function

--- libs/engine/src/test/integration.test.ts
+++ libs/engine/src/test/integration.test.ts
@@ -227,7 +227,14 @@ integrationTest('prime:range(2)', '[2]');
 integrationTest('prime:range(10)', '[2, 3, 5, 7]');
 integrationTest('prime:range(23)', '[2, 3, 5, 7, 11, 13, 17, 19, 23]');
 integrationTest('prime:nth(1)', '2');
+integrationTest('prime:nth(2)', '3');
 integrationTest('prime:nth(200)', '1223');
+integrationTest('prime:is(2)', 'true');
+integrationTest('prime:is(6)', 'false');
+integrationTest('prime:is(1223)', 'true');
+integrationTest('prime:is(1)', 'false');
+integrationTest('prime:is(1.3)', 'false');
+integrationTest('prime:is(-1)', 'false');
 
 integrationTestThrow('1 + true');
 integrationTestThrow('2 + [1,2,3]');
