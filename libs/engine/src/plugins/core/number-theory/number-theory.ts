c17c8121077610f75fb56eb6a409b52e96e93245
Tizian Roth <tizian.roth@freenet.de>
2022-10-30 22:52:37 +0100

    feat: add prime:pi function

--- libs/engine/src/plugins/core/number-theory/number-theory.ts
+++ libs/engine/src/plugins/core/number-theory/number-theory.ts
@@ -9,14 +9,18 @@ import { FunctionHeaderItem, NumberNode } from '../../../types/SyntaxTreeNodes';
 import { mapParametersToStackFrame } from '../../../utils/parameter-utils';
 import createNumberNode from '../../../create/create-number-node';
 import createVector from '../../../create/create-vector';
-import {getNthPrime, isPrime, sieveOfEratosthenes} from './utils/prime-number-utils';
-import createBooleanNode from "../../../create/create-boolean-node";
+import { getNthPrimeNumber, isPrimeNumber, sieveOfEratosthenes } from './utils/prime-number-utils';
+import createBooleanNode from '../../../create/create-boolean-node';
 
 const numberTheoryPlugin = createPlugin('core/number-theory');
 addPluginDescription(numberTheoryPlugin, 'brings many functions for calculation on integers and in number theory');
 addPluginAuthor(numberTheoryPlugin, 'core');
 
 const singleNumberHeader: FunctionHeaderItem[] = [{ name: 'n', type: 'number', evaluate: true }];
+const doubleNumberHeader: FunctionHeaderItem[] = [
+    { name: 'a', type: 'number', evaluate: true },
+    { name: 'a', type: 'number', evaluate: true },
+];
 
 addPluginFunction(
     numberTheoryPlugin,
@@ -26,14 +30,19 @@ addPluginFunction(
         'Returns all prime numbers between 2 and including n.',
         'Gibt alle Primzahlen zurück, die zwischen 2 und einschließlich n liegen.',
         (parameters, context) => {
-            const parameterStackFrame = mapParametersToStackFrame('prime:range', parameters, singleNumberHeader, context);
+            const parameterStackFrame = mapParametersToStackFrame(
+                'prime:range',
+                parameters,
+                singleNumberHeader,
+                context,
+            );
             const n = (<NumberNode>parameterStackFrame['n']).value;
 
             if (n % 1 !== 0) {
                 throw 'RuntimeError: prime:range: Only integers are allowed.';
             }
             if (n < 2) {
-                throw 'RuntimeError: prime:range: Numbers samaller than 2 are not allowed.';
+                throw 'RuntimeError: prime:range: Numbers smaller than 2 are not allowed.';
             }
             return createVector(sieveOfEratosthenes(n).map((value) => createNumberNode(value)));
         },
@@ -54,10 +63,10 @@ addPluginFunction(
             if (n % 1 !== 0) {
                 throw 'RuntimeError: prime:nth: Only integers are allowed.';
             }
-            if (n < 0) {
-                throw 'RuntimeError: prime:nth: Numbers samaller than 2 are not allowed.';
+            if (n < 1) {
+                throw 'RuntimeError: prime:nth: Numbers smaller than 1 are not allowed.';
             }
-            return createNumberNode(getNthPrime(n));
+            return createNumberNode(getNthPrimeNumber(n));
         },
     ),
 );
@@ -72,7 +81,30 @@ addPluginFunction(
         (parameters, context) => {
             const parameterStackFrame = mapParametersToStackFrame('prime:is', parameters, singleNumberHeader, context);
             const n = (<NumberNode>parameterStackFrame['n']).value;
-            return createBooleanNode(isPrime(n));
+            return createBooleanNode(isPrimeNumber(n));
+        },
+    ),
+);
+
+addPluginFunction(
+    numberTheoryPlugin,
+    createPluginFunction(
+        'prime:pi',
+        singleNumberHeader,
+        'The pi function specifies the number of prime numbers in the interval [1, n].',
+        'Die PiFunktion gibt die Anzahl der Primzahlen im Intervall (1, n] an.',
+        (parameters, context) => {
+            const parameterStackFrame = mapParametersToStackFrame('prime:pi', parameters, singleNumberHeader, context);
+            const n = (<NumberNode>parameterStackFrame['n']).value;
+
+            if (n % 1 !== 0) {
+                throw 'RuntimeError: prime:pi: Only integers are allowed.';
+            }
+            if (n < 2) {
+                throw 'RuntimeError: prime:pi: Numbers smaller than 2 are not allowed.';
+            }
+
+            return createNumberNode(sieveOfEratosthenes(n).length);
         },
     ),
 );
