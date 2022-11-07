33e9f251cdbae5d97b562b625acac6d5e7f27d76
Lennard Behrens <behrenle@posteo.de>
2022-11-07 09:30:47 +0100

    chore: change filename to main.js

--- libs/parser/build.js
+++ libs/parser/build.js
@@ -3,7 +3,7 @@ import fs from "fs";
 
 const pathToGrammar = "grammar.peggy";
 const outputDir = "lib";
-const outputPath = "lib/parser.js";
+const outputPath = "lib/main.js";
 const grammarSource = fs.readFileSync(pathToGrammar, "utf8");
 const parserSource = peggy.generate(grammarSource, {
   output: "source",
