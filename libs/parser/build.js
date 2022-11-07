835f176e03fb5808317c860a1fda32e560a86abb
Lennard Behrens <behrenle@posteo.de>
2022-11-07 09:29:26 +0100

    fix: fix dir naming

--- libs/parser/build.js
+++ libs/parser/build.js
@@ -1,17 +1,17 @@
-import peggy from 'peggy';
-import fs from 'fs';
+import peggy from "peggy";
+import fs from "fs";
 
-const pathToGrammar = 'grammar.peggy';
-const outputDir = 'generated';
-const outputPath = 'generated/parser.js';
-const grammarSource = fs.readFileSync(pathToGrammar, 'utf8');
+const pathToGrammar = "grammar.peggy";
+const outputDir = "lib";
+const outputPath = "lib/parser.js";
+const grammarSource = fs.readFileSync(pathToGrammar, "utf8");
 const parserSource = peggy.generate(grammarSource, {
-    output: 'source',
-    format: 'es',
-    cache: true,
+  output: "source",
+  format: "es",
+  cache: true,
 });
 
 if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
+  fs.mkdirSync(outputDir);
 }
 fs.writeFileSync(outputPath, parserSource);
