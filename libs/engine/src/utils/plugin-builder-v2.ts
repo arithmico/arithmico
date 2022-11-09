deca2f4419cb3bd1197a154ad287272083d631d6
Lennard Behrens <behrenle@posteo.de>
2022-11-09 12:10:52 +0100

    feat: new plugin builder api

--- libs/engine/src/utils/plugin-builder-v2.ts
+++ libs/engine/src/utils/plugin-builder-v2.ts
@@ -1,32 +1,185 @@
-import { SyntaxTreeNode } from '../types';
+import createFunctionCall from '../create/create-function-call';
+import createSymbolNode from '../create/create-symbol-node';
+import { Context, FunctionHeaderItem, FunctionNode, Plugin, SymbolNode, SyntaxTreeNode } from '../types';
+import { mapParametersToStackFrame } from './parameter-utils';
 
-interface HeaderItem<T extends string, U extends SyntaxTreeNode['type']> {
-    name: T;
-    type: U;
+function convertHeaderToSymbolList(header: FunctionHeaderItem[]): SymbolNode[] {
+    const result: SymbolNode[] = [];
+
+    header.forEach((item) => {
+        if (item.optional) {
+            result.push(createSymbolNode(item.name + '?'));
+        } else {
+            result.push(createSymbolNode(item.name));
+        }
+
+        if (item.repeat) {
+            result.push(createSymbolNode('...'));
+        }
+    });
+
+    return result;
 }
 
-type Header<T extends HeaderItem<string, SyntaxTreeNode['type']>[]> = T;
-
-function createHeaderItem<T extends string, U extends SyntaxTreeNode['type']>(name: T, type: U): HeaderItem<T> {
-    return { name, type };
+interface PluginFunctionProps {
+    getParameter(name: string): SyntaxTreeNode;
+    runtimeError(message: string): void;
+    typeError(message: string): void;
+    context: Context;
 }
 
-function createHeader<T extends HeaderItem<string, SyntaxTreeNode['type']>[]>(...items: T): Header<T> {
-    return items;
+export class PluginBuilderV2 {
+    private plugin: Plugin;
+
+    constructor() {
+        this.plugin = {
+            name: '',
+            author: '',
+            description: '',
+            constants: [],
+            functions: [],
+            inlineDefinitions: [],
+        };
+    }
+
+    withName(name: string): PluginBuilderV2 {
+        if (name.length < 1) {
+            throw 'invalid name';
+        }
+
+        if (this.plugin.name.length > 0) {
+            throw 'name is already set';
+        }
+
+        this.plugin.name = name;
+
+        return this;
+    }
+
+    withAuthor(author: string): PluginBuilderV2 {
+        if (author.length < 1) {
+            throw 'invalid author';
+        }
+
+        if (this.plugin.author.length > 0) {
+            throw 'author is already set';
+        }
+
+        this.plugin.author = author;
+
+        return this;
+    }
+
+    withDescription(description: string): PluginBuilderV2 {
+        if (description.length < 1) {
+            throw 'invalid description';
+        }
+
+        if (this.plugin.description.length > 0) {
+            throw 'description is already set';
+        }
+
+        this.plugin.description = description;
+
+        return this;
+    }
+
+    addConstant(
+        name: string,
+        descriptionEn: string,
+        descriptionDe: string,
+        value: Exclude<SyntaxTreeNode, FunctionNode>,
+    ): PluginBuilderV2 {
+        this.plugin.constants.push({
+            name,
+            value,
+            documentation: {
+                en: {
+                    synopsis: name,
+                    description: descriptionEn,
+                },
+                de: {
+                    synopsis: name,
+                    description: descriptionDe,
+                },
+            },
+        });
+
+        return this;
+    }
+
+    addFunction(
+        name: string,
+        header: FunctionHeaderItem[],
+        descriptionEn: string,
+        descriptionDe: string,
+        evaluator: (props: PluginFunctionProps) => SyntaxTreeNode,
+    ): PluginBuilderV2 {
+        const headerSymbols = convertHeaderToSymbolList(header);
+        const synopsis = `${name}(${headerSymbols.map((symbol) => symbol.name).join(', ')})`;
+        const expression = createFunctionCall(createSymbolNode(name), headerSymbols);
+
+        this.plugin.functions.push({
+            name,
+            documentation: {
+                en: {
+                    synopsis,
+                    description: descriptionEn,
+                },
+                de: {
+                    synopsis: synopsis.replace(',', ';').replace('.', ','),
+                    description: descriptionDe,
+                },
+            },
+            function: {
+                type: 'function',
+                expression,
+                header,
+                evaluator: (parameters, context) => {
+                    const parameterStackFrame = mapParametersToStackFrame(name, parameters, header, context);
+                    const getParameter = (name: string, fallback?: SyntaxTreeNode) => {
+                        const parameter = parameterStackFrame.get(name);
+
+                        if (!parameter && !fallback) {
+                            throw `RuntimeError: ${name}: No parameter fallback available`;
+                        }
+
+                        if (!parameter) {
+                            return fallback;
+                        }
+
+                        return parameter;
+                    };
+
+                    const runtimeError = (message: string) => {
+                        throw `RuntimeError: ${name}: ${message}`;
+                    };
+
+                    const typeError = (message: string) => {
+                        throw `TypeError: ${name}: ${message}`;
+                    };
+
+                    return evaluator({ getParameter, runtimeError, typeError, context });
+                },
+            },
+        });
+
+        return this;
+    }
+
+    build(): Plugin {
+        if (!this.plugin.name) {
+            throw 'no plugin name specified';
+        }
+
+        if (!this.plugin.author) {
+            throw 'no plugin author specified';
+        }
+
+        if (!this.plugin.description) {
+            throw 'no plugin description specified';
+        }
+
+        return this.plugin;
+    }
 }
-
-const testHeader = createHeader(
-    createHeaderItem('foo', 'number'),
-    createHeaderItem('bar', 'boolean'),
-    createHeaderItem('test', 'equals'),
-);
-
-function testFunc<T extends Header<HeaderItem<string, SyntaxTreeNode['type']>[]>>(
-    _header: T,
-    name: T[number]['name'],
-    type: T[number]['type'],
-) {
-    return name + type;
-}
-
-testFunc<typeof testHeader>(testHeader, 'foo', 'number');
