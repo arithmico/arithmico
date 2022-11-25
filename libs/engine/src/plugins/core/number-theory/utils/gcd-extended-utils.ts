61aabadb1249e35b3b0b6c63ce867d2d0c0ee311
Tizian Roth <tizian.roth@freenet.de>
2022-11-25 19:42:50 +0100

    fix: remove number-theory directory

--- libs/engine/src/plugins/core/number-theory/utils/gcd-extended-utils.ts
+++
@@ -1,8 +0,0 @@
-export function extendedEuclideanGreatestCommonDivisor(a: number, b: number): [number, number, number] {
-    if (b === 0) {
-        return [a, 1, 0];
-    }
-
-    const [gcd, bezoutCoefficientS, bezoutCoefficientT] = extendedEuclideanGreatestCommonDivisor(b, a % b);
-    return [gcd, bezoutCoefficientT, bezoutCoefficientS - Math.floor(a / b) * bezoutCoefficientT]; // euclidean division
-}
