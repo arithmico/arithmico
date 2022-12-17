010352e48bc29c3844759d937843b4a8aa18f634
Lennard Behrens <behrenle@posteo.de>
2022-12-17 20:25:00 +0100

    feat: use pipe for evaluation

---
+++ libs/engine/src/evaluation-pipeline.ts
@@ -0,0 +1,51 @@
+import { parse } from '@arithmico/parser';
+import { Context, SyntaxTreeNode } from './types';
+import evaluateNode from './eval';
+import { insertStackObject } from './utils/context-utils';
+import serialize from './serialize';
+import { pipe } from './utils/pipe';
+
+function pipeParseInput({ input, context }: { input: string; context: Context }) {
+    try {
+        const node = parse(input, {
+            language: context.options.decimalSeparator === ',' ? 'de' : 'en',
+        }) as SyntaxTreeNode;
+
+        return {
+            node,
+            context,
+        };
+    } catch (parseError) {
+        throw `SyntaxError: ${parseError.message}`;
+    }
+}
+
+function pipeEvaluateNode({ node, context }: { node: SyntaxTreeNode; context: Context }) {
+    const evaluatedNode = evaluateNode(node, context);
+    return {
+        node: evaluatedNode,
+        context,
+    };
+}
+
+function pipeModifyContext({ node, context }: { node: SyntaxTreeNode; context: Context }) {
+    if (node.type !== 'define') {
+        return { node, context };
+    }
+
+    return {
+        node,
+        context: insertStackObject(node.name, node.value, context),
+    };
+}
+
+function pipeSerialize({ node, context }: { node: SyntaxTreeNode; context: Context }) {
+    return {
+        result: serialize(node, context.options),
+        context,
+    };
+}
+
+const evaluateInput = pipe(pipeParseInput, pipeEvaluateNode, pipeModifyContext, pipeSerialize);
+
+export default evaluateInput;
