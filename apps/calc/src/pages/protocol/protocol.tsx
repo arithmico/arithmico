0ae0c03c679b97ce48f88583de3a37664ff41ba0
Lennard Behrens <behrenle@posteo.de>
2022-12-23 17:04:54 +0100

    feat: new protocol container

--- apps/calc/src/pages/protocol/protocol.tsx
+++ apps/calc/src/pages/protocol/protocol.tsx
@@ -1,133 +1,89 @@
-import React, { useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
 import PageContainer from "@local-components/page-container/page-container";
 import ProtocolMathItem from "@local-components/protocol-math-item/protocol-math-item";
 import { useSelector } from "react-redux";
 import { CalculatorRootState } from "@stores/calculator-store";
 import ProtocolInfoItem from "@local-components/protocol-info-item/info-protocol-item";
 import ProtocolErrorItem from "@local-components/protocol-error-item/protocol-error-item";
-
-const Container = styled(PageContainer)`
-  display: grid;
-  grid-template-rows: auto 1fr;
-  grid-gap: 1em;
-  padding-bottom: 0;
-`;
-
-const Header = styled.div`
-  display: flex;
-`;
-
-const Title = styled.h1`
-  font-size: 2.5em;
-  font-weight: var(--me-font-weight-normal);
-  margin: 0;
-`;
-
-const BackButton = styled.button`
-  margin-left: auto;
-  color: inherit;
-  background-color: var(--me-background-100);
-  outline: none;
-  border: none;
-  font-size: 2em;
-  padding: 0.25em 1em;
-  border-radius: 0.25em;
-
-  &:disabled {
-    color: var(--me-text-200);
-  }
-
-  &:enabled:hover {
-    background-color: var(--me-background-300);
-  }
-`;
-
-const HistoryContainer = styled.ul`
-  display: flex;
-  flex-direction: column;
-  margin: 0;
-  padding: 0;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  -ms-overflow-style: none; /* for Internet Explorer, Edge */
-  scrollbar-width: none; /* for Firefox */
-
-  &::-webkit-scrollbar {
-    display: none; /* for Chrome, Safari, and Opera */
-  }
-
-  & > li {
-    margin-bottom: 1em;
-  }
-
-  & > li:first-child {
-    margin-top: 2em;
-  }
-`;
+import WithScrollbars from "../../components/with-scrollbars/with-scrollbars";
+import classNames from "classnames";
 
 export default function Protocol() {
   const navigate = useNavigate();
-  const containerRef = useRef<HTMLUListElement>(null);
   const [t] = useTranslation();
-  useEffect(() => {
-    if (!containerRef.current) {
-      return;
-    }
-
-    if (containerRef.current.children.length === 0) {
-      return;
-    }
-
-    containerRef.current.children[
-      containerRef.current.children.length - 1
-    ].scrollIntoView({
-      behavior: "smooth",
-    });
-  }, [containerRef]);
 
   const protocolItems = useSelector(
     (state: CalculatorRootState) => state.session.protocol
   );
 
   return (
-    <Container>
-      <Header>
-        <Title>{t("protocol")}</Title>
-        <BackButton onClick={() => navigate("/")}>
-          {t("common.back")}
-        </BackButton>
-      </Header>
-      <HistoryContainer ref={containerRef}>
-        {protocolItems.map((item, index) => {
-          switch (item.type) {
-            case "math":
-              return (
-                <ProtocolMathItem
-                  input={item.input}
-                  output={item.output}
-                  key={index}
-                />
-              );
+    <PageContainer className={classNames("flex", "flex-col")}>
+      <div
+        className={classNames(
+          "grid",
+          "grid-rows-[auto_1fr]",
+          "theme-dark:bg-neutral-850",
+          "border",
+          "theme-dark:border-white/5",
+          "rounded-md",
+          "h-full"
+        )}
+      >
+        <div
+          className={classNames(
+            "flex",
+            "theme-dark:bg-neutral-800",
+            "rounded-md",
+            "items-center",
+            "ring-1",
+            "ring-white/5"
+          )}
+        >
+          <h1 className={classNames("text-3xl", "px-4")}>{t("protocol")}</h1>
+          <button
+            className={classNames(
+              "ml-auto",
+              "p-6",
+              "theme-dark:hover:bg-neutral-700",
+              "rounded-md"
+            )}
+            onClick={() => navigate("/")}
+          >
+            {t("common.back")}
+          </button>
+        </div>
+        <WithScrollbars>
+          <ul>
+            {protocolItems.map((item, index) => {
+              switch (item.type) {
+                case "math":
+                  return (
+                    <ProtocolMathItem
+                      input={item.input}
+                      output={item.output}
+                      key={index}
+                    />
+                  );
 
-            case "info":
-              return <ProtocolInfoItem message={item.info} key={index} />;
+                case "info":
+                  return <ProtocolInfoItem message={item.info} key={index} />;
 
-            case "error":
-              return (
-                <ProtocolErrorItem
-                  input={item.input}
-                  output={item.error}
-                  key={index}
-                />
-              );
-            default:
-              return null;
-          }
-        })}
-      </HistoryContainer>
-    </Container>
+                case "error":
+                  return (
+                    <ProtocolErrorItem
+                      input={item.input}
+                      output={item.error}
+                      key={index}
+                    />
+                  );
+                default:
+                  return null;
+              }
+            })}
+          </ul>
+        </WithScrollbars>
+      </div>
+    </PageContainer>
   );
 }
