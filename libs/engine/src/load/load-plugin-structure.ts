fd6bd8c223397ee1b8a185ea4bef84a602ab7362
Lennard Behrens <behrenle@posteo.de>
2022-12-25 16:45:52 +0100

    feat: new plugin structure type

--- libs/engine/src/load/load-plugin-structure.ts
+++ libs/engine/src/load/load-plugin-structure.ts
@@ -1,36 +1,69 @@
-import { Plugin } from '../types';
+import { Language, Plugin } from '../types';
 
 interface PluginItemStructure {
+    type: 'function' | 'constant' | 'method';
     name: string;
-    synopsis: string;
+    synopsis: {
+        [key in Language]: string;
+    };
     description: {
-        [key: string]: string;
+        [key in Language]?: string;
     };
 }
 
 export interface PluginStructure {
-    name: string;
+    name: {
+        [key in Language]: string;
+    };
     items: PluginItemStructure[];
 }
 
-export default function loadPluginStructures(plugins: Plugin[]): PluginStructure[] {
-    return plugins.map((plugin) => ({
-        name: plugin.name,
-        items: [
-            ...plugin.constants.map((pluginConstant) => ({
-                name: pluginConstant.name,
-                synopsis: pluginConstant.documentation.en.synopsis,
-                description: {
-                    en: pluginConstant.documentation.en.description,
-                },
-            })),
-            ...plugin.functions.map((pluginFunction) => ({
-                name: pluginFunction.name,
-                synopsis: pluginFunction.documentation.en.synopsis,
-                description: {
-                    en: pluginFunction.documentation.en.description,
-                },
-            })),
-        ],
+function loadPluginStructure(plugin: Plugin): PluginStructure {
+    const constants: PluginItemStructure[] = plugin.constants.map((constant) => ({
+        type: 'constant',
+        name: constant.name,
+        synopsis: {
+            en: constant.documentation.en.synopsis,
+            de: constant.documentation.de.synopsis,
+        },
+        description: {
+            en: constant.documentation.en.description,
+            de: constant.documentation.de.description,
+        },
     }));
+
+    const functions: PluginItemStructure[] = plugin.functions.map((pluginFunction) => ({
+        type: 'function',
+        name: pluginFunction.name,
+        synopsis: {
+            en: pluginFunction.documentation.en.synopsis,
+            de: pluginFunction.documentation.de.synopsis,
+        },
+        description: {
+            en: pluginFunction.documentation.en.description,
+            de: pluginFunction.documentation.de.description,
+        },
+    }));
+
+    const methods: PluginItemStructure[] = plugin.methods.map((method) => ({
+        type: 'method',
+        name: method.name,
+        synopsis: {
+            en: method.documentation.en.synopsis,
+            de: method.documentation.de.synopsis,
+        },
+        description: {
+            en: method.documentation.en.description,
+            de: method.documentation.de.description,
+        },
+    }));
+
+    return {
+        name: plugin.name,
+        items: [...constants, ...functions, ...methods],
+    };
+}
+
+export default function loadPluginStructures(plugins: Plugin[]): PluginStructure[] {
+    return plugins.map(loadPluginStructure);
 }
