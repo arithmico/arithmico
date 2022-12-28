34e026ec025e82155009cbc88eb660cad4c3a28d
Lennard Behrens <behrenle@posteo.de>
2022-12-28 11:46:29 +0100

    refactor: extract calculator-input component

--- apps/calc/src/components/calculator-form/calculator-form.tsx
+++ apps/calc/src/components/calculator-form/calculator-form.tsx
@@ -1,28 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
-import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
-import { CalculatorRootState } from "@stores/calculator-store";
-import {
-  moveForwardInHistory,
-  moveBackInHistory,
-  setInput,
-  resetInput,
-  resetOutput,
-} from "@stores/slices/calculator-session";
-import useEvaluate from "../../hooks/use-evaluate";
+import { resetInput, resetOutput } from "@stores/slices/calculator-session";
 import classNames from "classnames";
 import CalculatorOutput from "../calculator-output/calculator-output";
+import CalculatorInput from "../calculator-input/calculator-input";
+import { useDispatch } from "react-redux";
 
 export default function CalculatorForm() {
   const dispatch = useDispatch();
-  const evaluate = useEvaluate();
-  const input = useSelector(
-    (state: CalculatorRootState) => state.session.input
-  );
   const inputRef = useRef<HTMLInputElement>(null);
   const outputRef = useRef<HTMLInputElement>(null);
-  const [t] = useTranslation();
   const [focusNext, setFocusNext] = useState<"input" | "output" | null>(null);
 
   useEffect(() => {
@@ -80,23 +67,6 @@ export default function CalculatorForm() {
     { enableOnTags: ["INPUT"] }
   );
 
-  const onInputKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter" && input.length > 0) {
-      evaluate();
-      setFocusNext("output");
-    }
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "ArrowUp") {
-      dispatch(moveBackInHistory());
-      e.preventDefault();
-    } else if (e.key === "ArrowDown") {
-      dispatch(moveForwardInHistory());
-      e.preventDefault();
-    }
-  };
-
   return (
     <div
       className={classNames(
@@ -107,37 +77,14 @@ export default function CalculatorForm() {
         "justify-center"
       )}
     >
-      <input
-        type="text"
-        className={classNames(
-          "w-full",
-          "text-4xl",
-          "outline-none",
-          "border",
-          "px-4",
-          "py-6",
-          "rounded-md",
-          "bold-font:font-bold",
-          "theme-light:border-neutral-400",
-          "theme-light:focus:border-neutral-600",
-          "theme-light:bg-neutral-100",
-          "theme-dark:bg-neutral-800",
-          "theme-dark:border-neutral-500",
-          "theme-dark:focus:border-neutral-100"
-        )}
+      <CalculatorInput
         ref={inputRef}
-        placeholder={t("common.input")}
-        value={input}
-        onChange={(e) => dispatch(setInput(e.target.value))}
-        onKeyPress={onInputKeyPress}
-        onKeyDown={(e) => handleKeyDown(e)}
+        onEnterPressed={() => setFocusNext("output")}
       />
 
       <CalculatorOutput
         ref={outputRef}
-        onEnterPressed={() => {
-          setFocusNext("input");
-        }}
+        onEnterPressed={() => setFocusNext("input")}
       />
     </div>
   );
