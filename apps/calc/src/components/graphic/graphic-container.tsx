47f9aab0abd169b4939c12b3f621fc4da5830513
Tizian Roth <tizian.roth@freenet.de>
2023-03-02 21:38:15 +0100

    chore: fix lint

--- apps/calc/src/components/graphic/graphic-container.tsx
+++ apps/calc/src/components/graphic/graphic-container.tsx
@@ -18,6 +18,7 @@ export default function GraphicContainer({ graphic }: GraphicContainerProps) {
       setViewBoxHeight(ref.current.offsetHeight);
     }
   };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(setDimensions, [ref.current]);
 
   return (
