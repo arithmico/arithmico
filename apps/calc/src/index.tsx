b2477ece98ee8c3404b6b922903521cbcd2a9e00
Lennard Behrens <behrenle@posteo.de>
2023-03-15 22:42:12 +0100

    fix: fix react 18 create root warning

--- apps/calc/src/index.tsx
+++ apps/calc/src/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import "overlayscrollbars/css/OverlayScrollbars.css";
 import OverlayScrollbars from "overlayscrollbars";
@@ -8,18 +7,20 @@ import { Provider } from "react-redux";
 import calculatorStore from "@stores/calculator-store";
 import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
+import { createRoot } from "react-dom/client";
 
 const persistor = persistStore(calculatorStore);
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={calculatorStore}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 OverlayScrollbars(document.body, {
