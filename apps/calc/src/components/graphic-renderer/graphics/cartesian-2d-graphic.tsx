aea7d7c71c59b1840d47feed6c88c3c53ce2a57c
Lennard Behrens <behrenle@posteo.de>
2023-03-17 22:13:30 +0100

    feat: extract calculate auto ticks function

--- apps/calc/src/components/graphic-renderer/graphics/cartesian-2d-graphic.tsx
+++ apps/calc/src/components/graphic-renderer/graphics/cartesian-2d-graphic.tsx
@@ -1,17 +1,7 @@
 import { GraphicNode } from "@arithmico/engine/lib/types";
 import CoordinateGrid from "../components/coordinate-grid";
 import { GraphicDimensions, RenderTarget } from "../graphic-renderer.types";
-
-const autoTickSizes = [1, 2, 2.5, 5];
-const desiredXTickCount = 10;
-
-function applyMagnitudeToAutoTickSizes(m: number) {
-  return autoTickSizes.map((x) => x * Math.pow(10, m));
-}
-
-function getMagnitude(x: number) {
-  return x === 0 ? 0 : Math.floor(Math.log10(Math.abs(x)));
-}
+import { calculateAutoTicks } from "./calculate-auto-ticks";
 
 export interface Cartesian2DGraphicProps {
   target: RenderTarget;
@@ -24,27 +14,14 @@ export default function Cartesian2DGraphic({
   graphic,
   dimensions,
 }: Cartesian2DGraphicProps): JSX.Element {
-  const graphicWidth = graphic.limits.xMax - graphic.limits.xMin;
-  const graphicHeight = graphic.limits.yMax - graphic.limits.yMin;
-
-  const widthMagnitude = getMagnitude(graphicWidth);
-  const autoXTicks = [widthMagnitude - 1, widthMagnitude, widthMagnitude + 1]
-    .flatMap((m) => applyMagnitudeToAutoTickSizes(m))
-    .map((tickSize) => ({
-      tickSize,
-      desiredTickSizeError: Math.abs(
-        graphicWidth / tickSize - desiredXTickCount
-      ),
-    }))
-    .sort((x, y) => x.desiredTickSizeError - y.desiredTickSizeError)
-    .at(0)!.tickSize;
+  const ticks = calculateAutoTicks({ graphic, dimensions });
 
   return (
     <CoordinateGrid
       dimensions={dimensions}
       target={target}
       limits={graphic.limits}
-      ticks={{ xTicks: autoXTicks, yTicks: 2 }}
+      ticks={ticks}
     />
   );
 }
