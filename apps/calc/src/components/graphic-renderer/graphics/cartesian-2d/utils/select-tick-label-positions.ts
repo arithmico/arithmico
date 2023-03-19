c9da54eb09870403f4381499c18ffba76c671180
Lennard Behrens <behrenle@posteo.de>
2023-03-19 15:20:48 +0100

    chore: remove log

--- apps/calc/src/components/graphic-renderer/graphics/cartesian-2d/utils/select-tick-label-positions.ts
+++ apps/calc/src/components/graphic-renderer/graphics/cartesian-2d/utils/select-tick-label-positions.ts
@@ -69,7 +69,6 @@ export function selectTickLabelPositions({
   }
 
   if (tickLabelPositions.yAxis) {
-    console.log(tickLabelPositions.yAxis.tickLabelPositions);
     const allYAxisTicks = filterTickLabelCollisions({
       tickLabelPositions: tickLabelPositions.yAxis.tickLabelPositions,
       collisionPoints,
