1eed09cbf09571c883c9e68d2c0d7019b74880e9
Lennard Behrens <behrenle@posteo.de>
2023-03-19 16:05:52 +0100

    feat: better title text wrapping

--- apps/calc/src/components/pdf-graphic/components/pdf-export-title.tsx
+++ apps/calc/src/components/pdf-graphic/components/pdf-export-title.tsx
@@ -1,6 +1,22 @@
 import { View, Text } from "@react-pdf/renderer";
 import PdfArrowIcon from "./pdf-arrow-icon";
 
+function splitBeforeAndAfterPattern(str: string, pattern: string) {
+  return str
+    .split(pattern)
+    .flatMap((chunk, index) => (index === 0 ? [chunk] : [pattern, chunk]));
+}
+
+function splitBeforeAndAfterPatterns(str: string, patterns: string[]) {
+  return patterns
+    .reduce(
+      (chunks, pattern) =>
+        chunks.flatMap((chunk) => splitBeforeAndAfterPattern(chunk, pattern)),
+      [str]
+    )
+    .map((chunk) => chunk.trim());
+}
+
 export interface PdfExportTitleProps {
   title: string;
 }
@@ -16,14 +32,22 @@ export default function PdfExportTitle({ title }: PdfExportTitleProps) {
         alignItems: "center",
       }}
     >
-      {title.split("→").map((chunk, index) => {
-        return (
-          <>
-            {index !== 0 && <PdfArrowIcon />}
-            <Text style={{ fontSize: "26px" }}>{chunk}</Text>
-          </>
-        );
-      })}
+      {splitBeforeAndAfterPatterns(title, ["+", "-", "*", "/", "^", "→"]).map(
+        (chunk, index) =>
+          chunk === "→" ? (
+            <PdfArrowIcon />
+          ) : (
+            <Text
+              style={{
+                fontSize: "26px",
+                paddingLeft: "2px",
+                paddingRight: "2px",
+              }}
+            >
+              {chunk}
+            </Text>
+          )
+      )}
     </View>
   );
 }
