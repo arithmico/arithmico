aa4a5625039e99c04761b57881f56f8477590860
Lennard Behrens <behrenle@posteo.de>
2023-03-19 17:28:15 +0100

    feat: self collisions inside each axis

--- apps/calc/src/components/graphic-renderer/graphics/cartesian-2d/utils/compute-collisions.ts
+++ apps/calc/src/components/graphic-renderer/graphics/cartesian-2d/utils/compute-collisions.ts
@@ -1,5 +1,42 @@
 import { TickLabelPosition } from "./calculate-tick-label-positions";
 
+function check1DCollision(
+  x1: number,
+  w1: number,
+  x2: number,
+  w2: number
+): boolean {
+  return (x2 >= x1 && x2 <= x1 + w1) || (x2 + w2 >= x1 && x2 + w2 <= x1 + w1);
+}
+
+function checkSymmetric1DCollision(
+  x1: number,
+  w1: number,
+  x2: number,
+  w2: number
+): boolean {
+  return check1DCollision(x1, w1, x2, w2) || check1DCollision(x2, w2, x1, w1);
+}
+function checkHitboxCollision(
+  hitboxA: TickLabelPosition["hitbox"],
+  hitboxB: TickLabelPosition["hitbox"]
+): boolean {
+  return (
+    checkSymmetric1DCollision(
+      hitboxA.x,
+      hitboxA.wdith,
+      hitboxB.x,
+      hitboxB.wdith
+    ) &&
+    checkSymmetric1DCollision(
+      hitboxA.y,
+      hitboxA.height,
+      hitboxB.y,
+      hitboxB.height
+    )
+  );
+}
+
 export interface FilterTickLabelCollisionsArgs {
   tickLabelPositions: TickLabelPosition[];
   collisionPoints: { x: number; y: number }[];
@@ -9,7 +46,7 @@ export function filterTickLabelCollisions({
   tickLabelPositions,
   collisionPoints,
 }: FilterTickLabelCollisionsArgs) {
-  return tickLabelPositions.filter(({ hitbox }) => {
+  const filteredTicks = tickLabelPositions.filter(({ hitbox }) => {
     return !collisionPoints.some(({ x: px, y: py }) => {
       return (
         px >= hitbox.x &&
@@ -19,4 +56,15 @@ export function filterTickLabelCollisions({
       );
     });
   });
+  const result: TickLabelPosition[] = [];
+  filteredTicks.forEach((tick) => {
+    if (
+      !result.some((otherTick) =>
+        checkHitboxCollision(tick.hitbox, otherTick.hitbox)
+      )
+    ) {
+      result.push(tick);
+    }
+  });
+  return result;
 }
