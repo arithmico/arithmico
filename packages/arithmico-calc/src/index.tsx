2eef6a971f8870ecf3fe6183376234014ef30b2b
Lennard Behrens <behrenle@posteo.de>
2023-03-29 10:34:19 +0200

    refactor: move calculator store to arithmico-calc pacakge

--- packages/arithmico-calc/src/index.tsx
+++ packages/arithmico-calc/src/index.tsx
@@ -4,10 +4,10 @@ import "overlayscrollbars/css/OverlayScrollbars.css";
 import OverlayScrollbars from "overlayscrollbars";
 import "./index.css";
 import { Provider } from "react-redux";
-import calculatorStore from "@stores/calculator-store";
 import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
 import { createRoot } from "react-dom/client";
+import calculatorStore from "./store/store";
 
 const persistor = persistStore(calculatorStore);
 const container = document.getElementById("root")!;
