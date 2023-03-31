109508978100bf50c30b9d514dcc1c51fa4ad7a3
Lennard Behrens <behrenle@posteo.de>
2023-03-31 11:38:27 +0200

    feat: add new inline buttons

--- packages/frontend-components/src/index.ts
+++ packages/frontend-components/src/index.ts
@@ -5,4 +5,5 @@ export * from "./icons/expand-more.icon";
 export * from "./icons/arrow-back.icon";
 export * from "./icons/download.icon";
 export * from "./icons/delete.icon";
+export * from "./icons/close.icon";
 export * from "./imprint-content/imprint-content";
