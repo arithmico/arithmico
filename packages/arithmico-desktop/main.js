a4392dba87023178e2a812e9955b3b2fe64a423e
Lennard Behrens <behrenle@posteo.de>
2023-04-04 10:55:12 +0200

    feat: arithmico desktop mvp

---
+++ packages/arithmico-desktop/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "Arithmico Desktop",
+  });
+
+  win.loadFile("./web-dist/index.html").catch((e) => console.error(e));
+  win.setMenu(null);
+  win.maximize();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
