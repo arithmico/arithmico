fd9b4ae2805fea4e110ad0f797b31fe4f9c2a0bd
Lennard Behrens <behrenle@posteo.de>
2023-04-18 23:01:01 +0200

    feat: install mongoose module

--- packages/arithmico-api/src/app.module.ts
+++ packages/arithmico-api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [],
   providers: [],
 })
