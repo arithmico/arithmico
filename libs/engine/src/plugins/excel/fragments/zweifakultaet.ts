142120e6ef2c78e523bbc0d7e8cfddd04a04b413
staibflo-hspf <127963943+staibflo-hspf@users.noreply.github.com>
2023-04-23 23:28:18 +0200

    1. Idee zweifakultaet ungetestet

---
+++ libs/engine/src/plugins/excel/fragments/zweifakultaet.ts
@@ -0,0 +1,31 @@
+import { FunctionHeaderItem, NumberNode } from '../../../types/nodes.types';
+import createNumberNode from '../../../node-operations/create-node/create-number-node';
+import { PluginFragment } from '../../../utils/plugin-builder';
+
+const singleNumberHeader: FunctionHeaderItem[] = [{ name: 'n', type: 'number', evaluate: true }];
+
+const zweifakultaetFragment = new PluginFragment();
+__FUNCTIONS.zweifakultaet &&
+    zweifakultaetFragment.addFunction(
+        'zweifakultät',
+        singleNumberHeader,
+        'Calculates ...',
+        'Berechnet ...',
+        ({ getParameter, runtimeError }) => {
+            const n = (<NumberNode>getParameter('n')).value;
+            if (n < 0) {
+                throw runtimeError('Nur Ganzzahlen > 0 erlaubt.');
+            }
+            if (n % 1 !== 0) {
+                throw runtimeError('Keine Ganzzahl.');
+            }
+
+            let result = 1;
+            for (let i = 1; i <= n; i = i + 2) {
+                result *= i;
+            }
+            return createNumberNode(result);
+        },
+    );
+
+export default zweifakultaetFragment;
