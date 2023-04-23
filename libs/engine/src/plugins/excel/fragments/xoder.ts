ff58d6f768da599c54c789b69a7cfaddb8743743
staibflo-hspf <127963943+staibflo-hspf@users.noreply.github.com>
2023-04-23 22:55:38 +0200

    1. Idee xoder
    ungetestet, Frustration zu hoch

---
+++ libs/engine/src/plugins/excel/fragments/xoder.ts
@@ -0,0 +1,36 @@
+import createBooleanNode from '../../../node-operations/create-node/create-boolean-node';
+import { PluginFragment } from '../../../utils/plugin-builder';
+import { FunctionHeaderItem, NumberNode } from '../../../types/nodes.types';
+
+const booleanOperatorHeader: FunctionHeaderItem[] = [
+    {
+        name: 'a',
+        type: 'boolean',
+        evaluate: true,
+    },
+    {
+        name: 'b',
+        type: 'boolean',
+        evaluate: false,
+    },
+];
+
+const xoderFragment = new PluginFragment();
+__FUNCTIONS.xoder &&
+    xoderFragment.addFunction(
+        'xoder',
+        booleanOperatorHeader,
+        'Exclusive or, exactly one argument is true.',
+        'Ausschließendes Oder, genau ein Argument ist wahr.',
+        ({ getParameter, runtimeError }) => {
+            const a = (<NumberNode>getParameter('a')).value;
+            const b = (<NumberNode>getParameter('b')).value;
+            if (typeof a === 'boolean' && typeof b === 'boolean') {
+                return createBooleanNode(a !== b);
+            } else {
+                throw runtimeError('Mindestens ein Argument ist kein Wahrheitswert.');
+            }
+        },
+    );
+
+export default xoderFragment;
