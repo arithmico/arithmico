ea5198f97365d5d76e750aac43acf170ab83b110
staibflo-hspf <staibflo@hs-pforzheim.de>
2023-04-30 01:19:55 +0200

    Funktion erkennbar machen

--- libs/engine/src/plugins/excel/fragments/zweifakultaet.ts
+++ libs/engine/src/plugins/excel/fragments/zweifakultaet.ts
@@ -4,28 +4,26 @@ import { PluginFragment } from '../../../utils/plugin-builder';
 
 const singleNumberHeader: FunctionHeaderItem[] = [{ name: 'n', type: 'number', evaluate: true }];
 
-const zweifakultaetFragment = new PluginFragment();
-__FUNCTIONS.zweifakultaet &&
-    zweifakultaetFragment.addFunction(
-        'zweifakultät',
-        singleNumberHeader,
-        'Calculates ...',
-        'Berechnet ...',
-        ({ getParameter, runtimeError }) => {
-            const n = (<NumberNode>getParameter('n')).value;
-            if (n < 0) {
-                throw runtimeError('Nur Ganzzahlen > 0 erlaubt.');
-            }
-            if (n % 1 !== 0) {
-                throw runtimeError('Keine Ganzzahl.');
-            }
+const zweifakultaetFragment = new PluginFragment().addFunction(
+    'zweifakultät',
+    singleNumberHeader,
+    'Calculates ...',
+    'Berechnet ...',
+    ({ getParameter, runtimeError }) => {
+        const n = (<NumberNode>getParameter('n')).value;
+        if (n < 0) {
+            throw runtimeError('Nur Ganzzahlen > 0 erlaubt.');
+        }
+        if (n % 1 !== 0) {
+            throw runtimeError('Keine Ganzzahl.');
+        }
 
-            let result = 1;
-            for (let i = 1; i <= n; i = i + 2) {
-                result *= i;
-            }
-            return createNumberNode(result);
-        },
-    );
+        let result = 1;
+        for (let i = 1; i <= n; i = i + 2) {
+            result *= i;
+        }
+        return createNumberNode(result);
+    },
+);
 
 export default zweifakultaetFragment;
