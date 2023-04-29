dfb3833ba6adc870ceb310fb52bdc02e4db9023c
staibflo-hspf <127963943+staibflo-hspf@users.noreply.github.com>
2023-04-30 00:55:39 +0200

    Update excel.ts
    Lesbarkeit, Beschreibung des Plugins

--- libs/engine/src/plugins/excel/excel.ts
+++ libs/engine/src/plugins/excel/excel.ts
@@ -3,7 +3,17 @@ import excelConstantsFragment from './fragments/excel-constants';
 import xoderFragment from './fragments/xoder';
 import zweifakultaetFragment from './fragments/zweifakultaet';
 
-const excelPlugin = createPlugin({ en: 'Excel', de: 'Excel' }, 'core', { en: 'Test plugin.' })
+const excelPlugin = createPlugin(
+    {
+        en: 'Excel',
+        de: 'Excel',
+    },
+    'core',
+    {
+        en: 'Function names from Microsoft Excel, specifically of the german language.',
+        de: 'Funktionsnamen aus Microsoft Excel, speziell die der deutschen Sprache.',
+    },
+)
     .addFragment(excelConstantsFragment)
     .addFragment(xoderFragment)
     .addFragment(zweifakultaetFragment)
