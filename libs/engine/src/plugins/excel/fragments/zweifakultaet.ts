f821dbda490d539c1911ab7a97d2d50a4c33807c
staibflo-hspf <127963943+staibflo-hspf@users.noreply.github.com>
2023-04-30 20:21:56 +0200

    Revert "Verbesserung zweifakultät"
    This reverts commit 568537a86a2497e06aad15d5f054d3cd70a1f157.

--- libs/engine/src/plugins/excel/fragments/zweifakultaet.ts
+++ libs/engine/src/plugins/excel/fragments/zweifakultaet.ts
@@ -1,18 +1,28 @@
 import { FunctionHeaderItem, NumberNode } from '../../../types/nodes.types';
 import createNumberNode from '../../../node-operations/create-node/create-number-node';
 import { PluginFragment } from '../../../utils/plugin-builder';
-import { calculateFactdouble } from './utils/factdouble';
 
 const singleNumberHeader: FunctionHeaderItem[] = [{ name: 'n', type: 'number', evaluate: true }];
 
 const zweifakultaetFragment = new PluginFragment().addFunction(
-    'zweifakultaet', // should be zweifakultät TODO accept ä
+    'zweifakultät',
     singleNumberHeader,
-    'Calculates the factorial with a step of 2. Decimal places are cut off.',
-    'Gibt die Fakultät zu einer Zahl mit der Schrittlänge 2 zurück. Nachkommastellen werden abgeschnitten.',
-    ({ getParameter }) => {
+    'Calculates ...',
+    'Berechnet ...',
+    ({ getParameter, runtimeError }) => {
         const n = (<NumberNode>getParameter('n')).value;
-        return createNumberNode(calculateFactdouble(n));
+        if (n < 0) {
+            throw runtimeError('Nur Ganzzahlen > 0 erlaubt.');
+        }
+        if (n % 1 !== 0) {
+            throw runtimeError('Keine Ganzzahl.');
+        }
+
+        let result = 1;
+        for (let i = 1; i <= n; i = i + 2) {
+            result *= i;
+        }
+        return createNumberNode(result);
     },
 );
 
