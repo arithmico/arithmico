33a98c596062a7ea152ac5062eb657aac7f4e69e
Lennard Behrens <behrenle@posteo.de>
2023-05-16 11:39:17 +0200

    feat: create policy and group sub module

--- packages/arithmico-api/src/modules/security/security.module.ts
+++ packages/arithmico-api/src/modules/security/security.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PolicyModule } from './policy/policy.module';
+import { GroupModule } from './group/group.module';
 
-@Module({})
+@Module({
+  imports: [PolicyModule, GroupModule],
+})
 export class SecurityModule {}
