a945da8f4d28499d15278c3fa7b917aa5c4db011
Lennard Behrens <behrenle@posteo.de>
2023-06-01 14:32:11 +0200

    feat: add home title and description

--- packages/arithmico-manager/src/pages/home/home.tsx
+++ packages/arithmico-manager/src/pages/home/home.tsx
@@ -1,5 +1,16 @@
+import { FormattedMessage } from "react-intl";
+import Heading from "../../components/heading/heading";
 import { PageWithNavbar } from "../../components/page-with-navbar/page-with-navbar";
 
 export default function Home() {
-  return <PageWithNavbar>home</PageWithNavbar>;
+  return (
+    <PageWithNavbar>
+      <Heading level={1}>
+        <FormattedMessage id="home.title" />
+      </Heading>
+      <p>
+        <FormattedMessage id="home.description" />
+      </p>
+    </PageWithNavbar>
+  );
 }
