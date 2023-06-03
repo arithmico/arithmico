23922bb3cb8103ca1f40af728f0b58266b3c4a5d
Lennard Behrens <behrenle@posteo.de>
2023-06-03 13:06:33 +0200

    feat: finalize breadcrumbs design

--- packages/arithmico-manager/src/pages/security/pages/security-policy-details/security-policy-details.page.tsx
+++ packages/arithmico-manager/src/pages/security/pages/security-policy-details/security-policy-details.page.tsx
@@ -1,10 +1,16 @@
 import classNames from "classnames";
 import { FormattedMessage } from "react-intl";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { ChevronRightIcon } from "../../../../icons/chevron-right.icon";
 import { ShieldIcon } from "../../../../icons/shield.icon";
+import { useGetSecurityPolicyByIdQuery } from "../../../../store/api/slices/security/security.api";
 
 export function SecurityPolicyDetails() {
+  const { policyId } = useParams();
+  const { isSuccess, data } = useGetSecurityPolicyByIdQuery({
+    policyId: policyId!,
+  });
+
   return (
     <>
       <nav className={classNames("mt-4")}>
@@ -28,6 +34,14 @@ export function SecurityPolicyDetails() {
             <Link to="/security/security-policies">
               <FormattedMessage id="navbar.security.policies" />
             </Link>
+          </li>
+          <li
+            className={classNames("flex", "items-center", "text-neutral-500")}
+          >
+            <ChevronRightIcon
+              className={classNames("w-8", "h-8", "px-1", "fill-neutral-500")}
+            />
+            {data && data.name}
           </li>
         </ol>
       </nav>
