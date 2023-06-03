cf753df91e59cc8360f8a22ba228a6b1ad87f59c
Lennard Behrens <behrenle@posteo.de>
2023-06-03 13:01:29 +0200

    feat: add security policies page

--- packages/arithmico-manager/src/pages/security/pages/security/security.page.tsx
+++ packages/arithmico-manager/src/pages/security/pages/security/security.page.tsx
@@ -1,21 +1,9 @@
 import classNames from "classnames";
-import { useState } from "react";
 import { FormattedMessage } from "react-intl";
-import { Link } from "react-router-dom";
-import { Card } from "../../../../components/card/card";
 import Heading from "../../../../components/heading/heading";
-import { PaginationToolbar } from "../../../../components/pagination-toolbar/pagination-toolbar";
-import { Table } from "../../../../components/table/table";
-import { ChevronRightIcon } from "../../../../icons/chevron-right.icon";
-import { useGetSecurityPolicesQuery } from "../../../../store/api/slices/security/security.api";
+import { SecurityPoliciesPage } from "../security-policies/security-policies.page";
 
 export default function SecurityPage() {
-  const [skip, setSkip] = useState(0);
-  const { isSuccess, data } = useGetSecurityPolicesQuery({
-    skip,
-    limit: 100,
-  });
-
   return (
     <>
       <Heading level={1} className={classNames("mt-8")}>
@@ -24,54 +12,7 @@ export default function SecurityPage() {
       <p className={classNames("mb-4", "max-w-5xl")}>
         <FormattedMessage id="security.description" />
       </p>
-      <Heading level={2}>
-        <FormattedMessage id="security.policies.title" />
-      </Heading>
-      {isSuccess && data && (
-        <>
-          <Card>
-            <Table
-              header={[
-                {
-                  content: (
-                    <FormattedMessage id="security.policies.fields.id" />
-                  ),
-                },
-                {
-                  content: (
-                    <FormattedMessage id="security.policies.fields.name" />
-                  ),
-                },
-                {
-                  content: (
-                    <FormattedMessage id="security.policies.fields.attributes" />
-                  ),
-                },
-              ]}
-              rows={data.items.map((item) => [
-                item.id,
-                item.name,
-                item.attributes.length,
-                <Link
-                  to={`./security-policies/${item.id}`}
-                  className={classNames("float-right", "flex", "items-center")}
-                >
-                  <span className="sr-only">Details</span>
-                  <ChevronRightIcon
-                    className={classNames("w-8", "h-8", "fill-neutral-500")}
-                  />
-                </Link>,
-              ])}
-            />
-          </Card>
-          <PaginationToolbar
-            skip={data.skip}
-            limit={data.limit}
-            total={data.total}
-            onChange={setSkip}
-          />
-        </>
-      )}
+      <SecurityPoliciesPage />
     </>
   );
 }
--- packages/arithmico-manager/src/pages/security/security.router.tsx
+++ packages/arithmico-manager/src/pages/security/security.router.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { PageWithNavbar } from "../../components/page-with-navbar/page-with-navbar";
+import { SecurityPoliciesPage } from "./pages/security-policies/security-policies.page";
 import { SecurityPolicyDetails } from "./pages/security-policy-details/security-policy-details.page";
 import SecurityPage from "./pages/security/security.page";
 
@@ -8,6 +9,7 @@ export function SecurityRouter() {
     <PageWithNavbar>
       <Routes>
         <Route index element={<SecurityPage />} />
+        <Route path="security-policies" element={<SecurityPoliciesPage />} />
         <Route
           path="security-policies/:policyId"
           element={<SecurityPolicyDetails />}
