2c6721ea627ad0bb2e4f2730a9d6ea3d3bb39f0b
Lennard Behrens <dev.behrenle@posteo.de>
2023-06-10 23:12:03 +0200

    feat: add title and description

--- packages/arithmico-manager/src/pages/security/pages/user-groups/pages/create-user-group/create-user-group.page.tsx
+++ packages/arithmico-manager/src/pages/security/pages/user-groups/pages/create-user-group/create-user-group.page.tsx
@@ -1,10 +1,18 @@
+import classNames from "classnames";
+import { FormattedMessage } from "react-intl";
+import Heading from "../../../../../../components/heading/heading";
 import { CreateUserGroupBreadcrumbs } from "./components/create-user-group-breadcrumbs";
 
 export function CreateUserGroupPage() {
   return (
     <>
       <CreateUserGroupBreadcrumbs />
-      create user group
+      <Heading level={1} className={classNames("my-4")}>
+        <FormattedMessage id="security.user-groups.new.title" />
+      </Heading>
+      <p className={classNames("max-w-5xl")}>
+        <FormattedMessage id="security.user-groups.new.description" />
+      </p>
     </>
   );
 }
