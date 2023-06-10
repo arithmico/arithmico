a06c736ee70be5891bee808bdf76b59d5af2b80c
Lennard Behrens <dev.behrenle@posteo.de>
2023-06-10 17:10:20 +0200

    feat: add missing fields

--- packages/arithmico-manager/src/pages/security/pages/user-groups/user-groups.page.tsx
+++ packages/arithmico-manager/src/pages/security/pages/user-groups/user-groups.page.tsx
@@ -1,6 +1,11 @@
+import classNames from "classnames";
 import { useState } from "react";
+import { FormattedMessage } from "react-intl";
+import { ButtonLink } from "../../../../components/button-link/button-link";
 import { Card } from "../../../../components/card/card";
 import Heading from "../../../../components/heading/heading";
+import { PaginationToolbar } from "../../../../components/pagination-toolbar/pagination-toolbar";
+import { AddIcon } from "../../../../icons/add.icon";
 import { useGetUserGroupsQuery } from "../../../../store/api/resources/user-groups/user-groups.api";
 import { UserGroupsTable } from "./components/user-groups-table";
 
@@ -13,11 +18,30 @@ export function UserGroupsPage() {
 
   return (
     <>
-      <Heading level={1}>Benutzergruppen</Heading>
+      <div className={classNames("flex", "my-4")}>
+        <Heading level={1}>
+          <FormattedMessage id="security.user-groups.title" />
+        </Heading>
+        <ButtonLink
+          className={classNames("ml-auto")}
+          to="/security/user-groups/new"
+        >
+          <AddIcon className={classNames("w-6", "h-6", "fill-white", "mr-2")} />{" "}
+          <FormattedMessage id="security.user-groups.new" />
+        </ButtonLink>
+      </div>
       {isSuccess && data && (
-        <Card>
-          <UserGroupsTable userGroups={data.items} />
-        </Card>
+        <>
+          <Card>
+            <UserGroupsTable userGroups={data.items} />
+          </Card>
+          <PaginationToolbar
+            limit={data.limit}
+            skip={data.skip}
+            total={data.total}
+            onChange={setSkip}
+          />
+        </>
       )}
     </>
   );
