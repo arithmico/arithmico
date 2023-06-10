4733c6b1077d0c5c8c45c517adfaee5c565d8fe9
Lennard Behrens <dev.behrenle@posteo.de>
2023-06-10 16:01:40 +0200

    refactor: extract security policies table component

---
+++ packages/arithmico-manager/src/pages/security/pages/security-policies/components/security-policies-table.tsx
@@ -0,0 +1,74 @@
+import classNames from "classnames";
+import { FormattedMessage } from "react-intl";
+import { Link, useNavigate } from "react-router-dom";
+import { TableCell } from "../../../../../components/table-cell/table-cell";
+import { TableHeaderCell } from "../../../../../components/table-header-cell/table-header-cell";
+import { TableRow } from "../../../../../components/table-row/table-row";
+import { Table } from "../../../../../components/table/table";
+import { ChevronRightIcon } from "../../../../../icons/chevron-right.icon";
+import { SecurityPolicyDtoWithPrincipalsCount } from "../../../../../store/api/resources/security-policies/security-policies.types";
+
+export interface SecurityPoliciesTableProps {
+  policies: SecurityPolicyDtoWithPrincipalsCount[];
+}
+
+export function SecurityPoliciesTable({
+  policies,
+}: SecurityPoliciesTableProps) {
+  const navigate = useNavigate();
+
+  return (
+    <Table
+      className={classNames("table-fixed")}
+      header={
+        <>
+          <TableHeaderCell className={classNames("w-1/4")}>
+            <FormattedMessage id="security.policies.fields.id" />
+          </TableHeaderCell>
+          <TableHeaderCell className={classNames("w-1/5")}>
+            <FormattedMessage id="security.policies.fields.name" />
+          </TableHeaderCell>
+          <TableHeaderCell className={classNames("w-1/5")}>
+            <FormattedMessage id="security.policies.fields.attributes" />
+          </TableHeaderCell>
+          <TableHeaderCell>
+            <FormattedMessage id="security.policies.fields.principals" />
+          </TableHeaderCell>
+          <TableHeaderCell>
+            <FormattedMessage id="security.policies.fields.radonly" />
+          </TableHeaderCell>
+        </>
+      }
+    >
+      {policies.map((item, index) => (
+        <TableRow
+          onClick={() => navigate(`/security/security-policies/${item.id}`)}
+          key={index}
+        >
+          <TableCell>{item.id}</TableCell>
+          <TableCell>{item.name}</TableCell>
+          <TableCell>{item.attributes.length}</TableCell>
+          <TableCell>{item.principals}</TableCell>
+          <TableCell>
+            {item.readonly ? (
+              <FormattedMessage id="common.yes" />
+            ) : (
+              <FormattedMessage id="common.no" />
+            )}
+          </TableCell>
+          <TableCell>
+            <Link
+              to={`/security/security-policies/${item.id}`}
+              className={classNames("float-right", "flex", "items-center")}
+            >
+              <span className="sr-only">Details</span>
+              <ChevronRightIcon
+                className={classNames("w-8", "h-8", "fill-neutral-500")}
+              />
+            </Link>
+          </TableCell>
+        </TableRow>
+      ))}
+    </Table>
+  );
+}
--- packages/arithmico-manager/src/pages/security/pages/security-policies/security-policies.page.tsx
+++ packages/arithmico-manager/src/pages/security/pages/security-policies/security-policies.page.tsx
@@ -1,25 +1,20 @@
 import classNames from "classnames";
 import { useState } from "react";
 import { FormattedMessage } from "react-intl";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Card } from "../../../../components/card/card";
 import Heading from "../../../../components/heading/heading";
 import { PaginationToolbar } from "../../../../components/pagination-toolbar/pagination-toolbar";
-import { TableCell } from "../../../../components/table-cell/table-cell";
-import { TableHeaderCell } from "../../../../components/table-header-cell/table-header-cell";
-import { TableRow } from "../../../../components/table-row/table-row";
-import { Table } from "../../../../components/table/table";
 import { AddIcon } from "../../../../icons/add.icon";
-import { ChevronRightIcon } from "../../../../icons/chevron-right.icon";
 import { useGetSecurityPolicesQuery } from "../../../../store/api/resources/security-policies/security-policies.api";
 import { SecurityPoliciesBreadcrumbs } from "./components/security-policies-breacrumbs";
+import { SecurityPoliciesTable } from "./components/security-policies-table";
 
 export interface SecurityPoliciesPageProps {
   inline?: boolean;
 }
 
 export function SecurityPoliciesPage({ inline }: SecurityPoliciesPageProps) {
-  const navigate = useNavigate();
   const [skip, setSkip] = useState(0);
   const { isSuccess, data } = useGetSecurityPolicesQuery({
     skip,
@@ -57,70 +52,13 @@ export function SecurityPoliciesPage({ inline }: SecurityPoliciesPageProps) {
           )}
         >
           <AddIcon className={classNames("w-6", "h-6", "fill-white", "mr-2")} />{" "}
-          Hinzufügen
+          <FormattedMessage id="security.policies.add" />
         </Link>
       </div>
       {isSuccess && data && (
         <>
           <Card>
-            <Table
-              className={classNames("table-fixed")}
-              header={
-                <>
-                  <TableHeaderCell className={classNames("w-1/4")}>
-                    <FormattedMessage id="security.policies.fields.id" />
-                  </TableHeaderCell>
-                  <TableHeaderCell className={classNames("w-1/5")}>
-                    <FormattedMessage id="security.policies.fields.name" />
-                  </TableHeaderCell>
-                  <TableHeaderCell className={classNames("w-1/5")}>
-                    <FormattedMessage id="security.policies.fields.attributes" />
-                  </TableHeaderCell>
-                  <TableHeaderCell>
-                    <FormattedMessage id="security.policies.fields.principals" />
-                  </TableHeaderCell>
-                  <TableHeaderCell>
-                    <FormattedMessage id="security.policies.fields.radonly" />
-                  </TableHeaderCell>
-                </>
-              }
-            >
-              {data.items.map((item, index) => (
-                <TableRow
-                  onClick={() =>
-                    navigate(`/security/security-policies/${item.id}`)
-                  }
-                  key={index}
-                >
-                  <TableCell>{item.id}</TableCell>
-                  <TableCell>{item.name}</TableCell>
-                  <TableCell>{item.attributes.length}</TableCell>
-                  <TableCell>{item.principals}</TableCell>
-                  <TableCell>
-                    {item.readonly ? (
-                      <FormattedMessage id="common.yes" />
-                    ) : (
-                      <FormattedMessage id="common.no" />
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    <Link
-                      to={`/security/security-policies/${item.id}`}
-                      className={classNames(
-                        "float-right",
-                        "flex",
-                        "items-center"
-                      )}
-                    >
-                      <span className="sr-only">Details</span>
-                      <ChevronRightIcon
-                        className={classNames("w-8", "h-8", "fill-neutral-500")}
-                      />
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </Table>
+            <SecurityPoliciesTable policies={data.items} />
           </Card>
           <PaginationToolbar
             skip={data.skip}
