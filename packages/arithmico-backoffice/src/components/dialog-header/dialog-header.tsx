77dd353a3588e204426af46191948824f824eceb
Lennard Behrens <dev.behrenle@posteo.de>
2023-06-27 16:04:55 +0200

    chore: add sr only text to close button

--- packages/arithmico-backoffice/src/components/dialog-header/dialog-header.tsx
+++ packages/arithmico-backoffice/src/components/dialog-header/dialog-header.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { FormattedMessage } from "react-intl";
 import { CloseIcon } from "../../icons/close.icon";
 import Heading from "../heading/heading";
 
@@ -12,6 +13,9 @@ export function DialogHeader({ children, onClose }: DialogHeaderProps) {
     <div className="mb-4 flex items-center">
       <Heading level={2}>{children}</Heading>
       <button className="ml-auto" onClick={() => onClose()}>
+        <span className="sr-only">
+          <FormattedMessage id="common.close-dialog" />
+        </span>
         <CloseIcon className="h-6 w-6" />
       </button>
     </div>
