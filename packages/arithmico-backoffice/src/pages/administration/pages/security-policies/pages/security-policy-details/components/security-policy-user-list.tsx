838bf3b896b4efc4448fbab3dd23aab11454399a
Lennard Behrens <dev.behrenle@posteo.de>
2023-06-29 13:13:07 +0200

    feat(backoffice): :sparkles: add empty list fallback text

--- packages/arithmico-backoffice/src/pages/administration/pages/security-policies/pages/security-policy-details/components/security-policy-user-list.tsx
+++ packages/arithmico-backoffice/src/pages/administration/pages/security-policies/pages/security-policy-details/components/security-policy-user-list.tsx
@@ -51,26 +51,33 @@ export function SecurityPolicyUserList({
         </div>
         {isSuccess && (
           <>
-            <BoxedList>
-              {data.items.map((user) => {
-                const userUrl = `/administration/users/${user.id}`;
-                return (
-                  <BoxedList.Item
-                    key={user.id}
-                    className="py-2 pl-4 pr-2"
-                    onClick={() => navigate(userUrl)}
-                  >
-                    {user.username}
-                    <Link to={userUrl} className="ml-auto">
-                      <span className="sr-only">
-                        <FormattedMessage id="admin.security-policies.users.details" />
-                      </span>
-                      <ChevronRightIcon className="h-6 w-6 fill-black/50" />
-                    </Link>
-                  </BoxedList.Item>
-                );
-              })}
-            </BoxedList>
+            {data.items.length > 0 && (
+              <BoxedList>
+                {data.items.map((user) => {
+                  const userUrl = `/administration/users/${user.id}`;
+                  return (
+                    <BoxedList.Item
+                      key={user.id}
+                      className="py-2 pl-4 pr-2"
+                      onClick={() => navigate(userUrl)}
+                    >
+                      {user.username}
+                      <Link to={userUrl} className="ml-auto">
+                        <span className="sr-only">
+                          <FormattedMessage id="admin.security-policies.users.details" />
+                        </span>
+                        <ChevronRightIcon className="h-6 w-6 fill-black/50" />
+                      </Link>
+                    </BoxedList.Item>
+                  );
+                })}
+              </BoxedList>
+            )}
+            {data.items.length === 0 && (
+              <p className="rounded-sm border border-black/30 p-6 text-center text-black/30">
+                <FormattedMessage id="admin.security-policies.users.empty" />
+              </p>
+            )}
             {data.total > limit && (
               <PaginationToolbar
                 className="mt-4"
