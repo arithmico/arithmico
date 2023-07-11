1734ad5430841c90b98eadd7dce16694ffa440f4
Lennard Behrens <dev.behrenle@posteo.de>
2023-07-11 09:23:37 +0200

    feat(api): :sparkles: get version tag by id query

--- packages/arithmico-api/src/modules/version-tag/queries/index.ts
+++ packages/arithmico-api/src/modules/version-tag/queries/index.ts
@@ -1,5 +1,11 @@
+import { GetVersionTagByIdController } from './get-version-tag-by-id/get-version-tag-by-id.controller';
+import { GetVersionTagByIdHandler } from './get-version-tag-by-id/get-version-tag-by-id.handler';
 import { GetVersionTagsController } from './get-version-tags/get-version-tags.controller';
 import { GetVersionTagsHandler } from './get-version-tags/get-version-tags.handler';
 
-export const queryHandlers = [GetVersionTagsHandler];
-export const queryControllers = [GetVersionTagsController];
+export const queryHandlers = [GetVersionTagsHandler, GetVersionTagByIdHandler];
+
+export const queryControllers = [
+  GetVersionTagsController,
+  GetVersionTagByIdController,
+];
