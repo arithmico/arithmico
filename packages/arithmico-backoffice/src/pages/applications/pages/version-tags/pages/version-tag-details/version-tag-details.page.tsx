8a09bf36da58d5c22324978ab983e2ddc03e8e0d
Lennard Behrens <dev.behrenle@posteo.de>
2023-07-11 17:02:37 +0200

    feat(backoffice): :sparkles: add list of available feature flags to version tag details page

--- packages/arithmico-backoffice/src/pages/applications/pages/version-tags/pages/version-tag-details/version-tag-details.page.tsx
+++ packages/arithmico-backoffice/src/pages/applications/pages/version-tags/pages/version-tag-details/version-tag-details.page.tsx
@@ -3,6 +3,7 @@ import Heading from "../../../../../../components/heading/heading";
 import { SemanticVersion } from "../../../../../../components/semantic-version/semantic-version";
 import { useGetVersionTagByIdQuery } from "../../../../../../store/api/resources/version-tags/version-tags.api";
 import { VersionTagDetailsCard } from "./components/version-tag-details-card";
+import { VersionTagFeatureFlagList } from "./components/version-tag-feature-flag-list";
 
 export function VersionTagDetailsPage() {
   const { tagId } = useParams();
@@ -19,9 +20,12 @@ export function VersionTagDetailsPage() {
       <Heading level={1} className="my-4">
         <SemanticVersion version={data.version} />
       </Heading>
-      <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
         <div className="flex flex-col lg:col-start-2 lg:row-start-1">
           <VersionTagDetailsCard versionTag={data} />
+        </div>
+        <div className="flex flex-col">
+          <VersionTagFeatureFlagList tagId={tagId!} />
         </div>
       </div>
     </>
