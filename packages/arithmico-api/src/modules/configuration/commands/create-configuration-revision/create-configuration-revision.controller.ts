ad9b84b4f980694e3fdfcd8ad58c94b01650cb91
Lennard Behrens <dev.behrenle@posteo.de>
2023-07-22 20:05:14 +0200

    fix(api): :bug: add missing security attribute

--- packages/arithmico-api/src/modules/configuration/commands/create-configuration-revision/create-configuration-revision.controller.ts
+++ packages/arithmico-api/src/modules/configuration/commands/create-configuration-revision/create-configuration-revision.controller.ts
@@ -4,11 +4,14 @@ import { CreateConfigurationRevisionResponseDto } from './create-configuration-r
 import { CreateConfigurationRevisionRequestBodyDto } from './create-configuration-revision.request.body.dto';
 import { CreateConfigurationRevisionRequestParamsDto } from './create-configuration-revision.request.params.dto';
 import { CreateConfigurationRevisionCommand } from './create-configuration-revision.command';
+import { SecurityAttribute } from '../../../../common/constants/security-attributes.enum';
+import { SecurityAttributes } from '../../../../decorators/security-attributes.decorator';
 
 @Controller(':configurationId/revisions')
 export class CreateConfigurationRevisionController {
   constructor(private commandBus: CommandBus) {}
 
+  @SecurityAttributes(SecurityAttribute.ConfigurationRevisionsWrite)
   @Post()
   async createConfigurationRevision(
     @Param() params: CreateConfigurationRevisionRequestParamsDto,
