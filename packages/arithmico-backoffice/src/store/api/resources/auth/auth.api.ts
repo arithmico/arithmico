2ffc03d3d1b435a56768093bdeaac3907fd1971d
Lennard Behrens <dev.behrenle@posteo.de>
2023-07-25 17:37:16 +0200

    feat(backoffice): :sparkles: hide navbar items if permission requirements are not met

--- packages/arithmico-backoffice/src/store/api/resources/auth/auth.api.ts
+++ packages/arithmico-backoffice/src/store/api/resources/auth/auth.api.ts
@@ -1,16 +1,25 @@
 import { api } from "../../api";
-import { CredentialsDto, LoginDto } from "./auth.types";
+import { CredentialsDto, LoginDto, SecurityAttributesDto } from "./auth.types";
 
 const authApi = api.injectEndpoints({
   endpoints: (build) => ({
+    getSecurityAttributes: build.query<SecurityAttributesDto, void>({
+      query: () => ({
+        url: "/auth/security-attributes",
+        method: "GET",
+      }),
+      providesTags: ["SecurityAttribute"],
+    }),
+
     login: build.mutation<CredentialsDto, LoginDto>({
       query: (arg) => ({
         url: "/auth/login",
         method: "POST",
         body: arg,
       }),
+      invalidatesTags: ["SecurityAttribute"],
     }),
   }),
 });
 
-export const { useLoginMutation } = authApi;
+export const { useLoginMutation, useGetSecurityAttributesQuery } = authApi;
--- packages/arithmico-backoffice/src/store/api/resources/auth/auth.types.ts
+++ packages/arithmico-backoffice/src/store/api/resources/auth/auth.types.ts
@@ -7,3 +7,7 @@ export interface LoginDto {
   username: string;
   password: string;
 }
+
+export interface SecurityAttributesDto {
+  securityAttributes: string[];
+}
