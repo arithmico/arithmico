c9ead4fa287068f254b9d34b5d7c59dc7c03922e
Lennard Behrens <dev.behrenle@posteo.de>
2023-09-06 12:47:30 +0200

    fix(backoffice): :lipstick: remove conflicting class name

--- packages/applications/backoffice/src/icons/loading.icon.tsx
+++ packages/applications/backoffice/src/icons/loading.icon.tsx
@@ -12,7 +12,7 @@ export default function LoadingIcon({ className }: Props) {
       viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={classNames("animate-spin stroke-white", className)}
+      className={classNames("animate-spin", className)}
       aria-hidden
     >
       <path
