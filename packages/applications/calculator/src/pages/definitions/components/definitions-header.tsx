77d26af3722a3fc59e4ace25407458b54fb5c0da
Tizian Roth <tizian.roth@freenet.de>
2023-09-12 21:14:16 +0200

    feat: add outline in definitions

--- packages/applications/calculator/src/pages/definitions/components/definitions-header.tsx
+++ packages/applications/calculator/src/pages/definitions/components/definitions-header.tsx
@@ -21,7 +21,7 @@ export function DefinitionsHeader() {
         "items-center",
         "ring-1",
         "theme-dark:ring-white/5",
-        "theme-light:ring-black/10"
+        "theme-light:ring-black/10",
       )}
     >
       <h1
@@ -38,19 +38,27 @@ export function DefinitionsHeader() {
             "space-x-2",
             "px-2",
             "sm:px-4",
+            "mt-1",
+            "mr-1",
             "opacity-60",
             "hover:opacity-100",
             "rounded-md",
             "theme-dark:hover:bg-neutral-700",
-            "theme-light:hover:bg-neutral-300"
+            "theme-light:hover:bg-neutral-300",
+            "ui-focus-visible:outline",
+            "outline-2",
+            "outline-offset-2",
+            "theme-light:outline-black",
+            "theme-dark:outline-white",
           )}
         >
           <DeleteIcon
             className={classNames(
+              "justify-center",
               "w-6",
               "h-6",
               "theme-light:fill-black",
-              "theme-dark:fill-white"
+              "theme-dark:fill-white",
             )}
           />
           <span aria-hidden className={classNames("hidden", "sm:inline-block")}>
@@ -62,14 +70,21 @@ export function DefinitionsHeader() {
       <button
         className={classNames(
           "ml-auto",
-          "p-3  ",
+          "p-3",
+          "mt-1",
+          "ml-1",
           "theme-dark:hover:bg-neutral-700",
           "theme-light:hover:bg-neutral-300",
           "rounded-md",
           "col-start-1",
           "row-start-1",
           "opacity-60",
-          "hover:opacity-100"
+          "hover:opacity-100",
+          "ui-focus-visible:outline",
+          "outline-2",
+          "outline-offset-2",
+          "theme-light:outline-black",
+          "theme-dark:outline-white",
         )}
         onClick={() => navigate("/")}
       >
@@ -78,7 +93,7 @@ export function DefinitionsHeader() {
             "theme-dark:fill-white",
             "theme-light:fill-black",
             "w-6",
-            "h-6"
+            "h-6",
           )}
         />
         <span className="sr-only">{t("common.back")}</span>
