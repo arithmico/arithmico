bb3a297d3178c57e41ec509c22c075d74a2fc52f
tizian <tizian.roth@freenet.de>
2023-11-21 01:44:06 +0100

    fix: fraction precision

--- packages/libraries/engine/src/plugins/discrete-math/utils/fraction-utils.ts
+++ packages/libraries/engine/src/plugins/discrete-math/utils/fraction-utils.ts
@@ -1,5 +1,32 @@
+import { greatestCommonDivisor } from '../../../utils/math-utils/float-utils';
+
 export function getLowestFraction(x0: number) {
+    /*
     const eps = 1.0e-15;
+    let h, h1, h2, k, k1, k2, a, x;
+
+    x = x0;
+    a = Math.floor(x);
+    h1 = 1;
+    k1 = 0;
+    h = a;
+    k = 1;
+
+    while (x - a > eps * k * k) {
+        x = 1 / (x - a);
+        a = Math.floor(x);
+        h2 = h1;
+        h1 = h;
+        k2 = k1;
+        k1 = k;
+        h = h2 + a * h1;
+        k = k2 + a * k1;
+    }
+
+    return [h, k];
+     */
+
+    const eps = 1.0e-5;
     let h, h1, h2, k, k1, k2, a, x;
 
     x = x0;
