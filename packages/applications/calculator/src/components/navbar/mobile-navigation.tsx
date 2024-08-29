84900c89cf602484295cd82a83dc770043554691
tizian <tizian.roth@freenet.de>
2024-08-29 05:19:21 +0200

    feat: make mobile menu accessible with keyboard

--- packages/applications/calculator/src/components/navbar/mobile-navigation.tsx
+++ packages/applications/calculator/src/components/navbar/mobile-navigation.tsx
@@ -1,11 +1,10 @@
 import { MenuIcon } from "ui-components";
 import { Menu } from "@headlessui/react";
 import classNames from "classnames";
-import { Fragment, useEffect } from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
 import { NavigationProps } from "./types";
-import { NavigationLink } from "@local-components/navbar/navigation-link";
 
 interface DisclosureContentProps {
   items: NavigationProps["items"];
@@ -28,8 +27,12 @@ function MenuContent({ items, close }: DisclosureContentProps) {
           "outline-none",
           "flex",
           "items-center",
-          "-mr-4",
+          "-mr-2",
           "px-2",
+          "ui-focus-visible:outline-none",
+          "ui-focus-visible:outline-offset-2",
+          "ui-focus-visible:theme-light:outline-black",
+          "ui-focus-visible:theme-dark:outline-white",
         )}
       >
         <MenuIcon
@@ -38,36 +41,54 @@ function MenuContent({ items, close }: DisclosureContentProps) {
             "theme-dark:fill-white",
             "w-8",
             "h-8",
-            "ui-focus-visible:outline",
-            "ui-focus-visible:outline-offset-2",
-            "ui-focus-visible:theme-light:outline-black",
-            "ui-focus-visible:theme-dark:outline-white",
           )}
         />
         <span className="sr-only">{t("nav.menu")}</span>
       </Menu.Button>
-      <Menu.Items>
-        <nav
-          className={classNames(
-            "absolute",
-            "left-0",
-            "right-0",
-            "[&_li]:border-b",
-            "[&_li]:border-b-neutral-600",
-            "theme-light:bg-neutral-300",
-            "theme-dark:bg-neutral-800",
-          )}
-        >
-          {items.map((link, index) => (
-            <Menu.Item key={link.to} as={Fragment}>
-              {({ active }) => (
-                <div className={active ? "bg-blue-500" : "bg-red-500"}>
-                  <NavigationLink to={link.to}>{link.label}</NavigationLink>
-                </div>
+      <Menu.Items
+        as="nav"
+        className={classNames(
+          "absolute",
+          "left-0",
+          "right-0",
+          "z-0",
+          "mt-[30%]",
+          "outline-none",
+          "[&:hover>div>div]:outline-none",
+          "border",
+          "theme-light:bg-neutral-300",
+          "theme-light:border-neutral-700",
+          "theme-dark:bg-neutral-800",
+          "theme-dark:border-neutral-300",
+        )}
+      >
+        {items.map((link) => (
+          <Menu.Item
+            as="div"
+            className={classNames(
+              "block",
+              "p-2",
+              "border-b",
+              "theme-light:border-b-neutral-700",
+              "theme-dark:border-b-neutral-300",
+            )}
+          >
+            <div
+              className={classNames(
+                "mx-1",
+                "pl-1",
+                "ui-active:outline",
+                "outline-offset-2",
+                "theme-light:outline-black",
+                "theme-dark:outline-white",
               )}
-            </Menu.Item>
-          ))}
-        </nav>
+            >
+              <a key={link.to} href={link.to}>
+                {link.label}
+              </a>
+            </div>
+          </Menu.Item>
+        ))}
       </Menu.Items>
     </>
   );
