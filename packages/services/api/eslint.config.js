61f19f48e3303b55b2069fd0dd5ce3794089e191
Lennard Behrens <dev.behrenle@posteo.de>
2025-02-25 13:33:36 +0100

    fix: :bug: rename eslint config file

---
+++ packages/services/api/eslint.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
